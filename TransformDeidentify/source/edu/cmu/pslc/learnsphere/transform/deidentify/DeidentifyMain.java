8dad8c1c28bba442eee42dd9a3d5b413bd2c05db
Cindy Tipper <ctipper@cs.cmu.edu>
2017-03-14 15:39:45 -0400

    #816 (Workflows: preview for Deidentify output not displaying correctly)

--- TransformDeidentify/source/edu/cmu/pslc/learnsphere/transform/deidentify/DeidentifyMain.java
+++ TransformDeidentify/source/edu/cmu/pslc/learnsphere/transform/deidentify/DeidentifyMain.java
@@ -260,11 +260,11 @@ public class DeidentifyMain extends AbstractComponent {
         //deidentify file
         deidentifyFile(studentIdPairsOfFile, deidentifiedFile);
 
+        Integer fileIndex = 0;
         Integer nodeIndex = 0;
-        Integer fileIndex0 = 0;
-        Integer fileIndex1 = 1;
-        this.addOutputFile(newUserMapFile, nodeIndex, fileIndex0, "user-map");
-        this.addOutputFile(deidentifiedFile, nodeIndex, fileIndex1, "tab-delimited");
+        this.addOutputFile(newUserMapFile, nodeIndex, fileIndex, "user-map");
+        nodeIndex = 1;
+        this.addOutputFile(deidentifiedFile, nodeIndex, fileIndex, "tab-delimited");
 
         System.out.println(this.getOutput());
     }
