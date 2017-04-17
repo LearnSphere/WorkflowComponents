6dbebf55101cf20b4d5753a2d6160ad5eaac02a6
Mike Komisin <mkomisin@cs.cmu.edu>
2017-04-17 07:28:09 -0400

    Import Text component.

---
+++ ImportText/source/edu/cmu/pslc/learnsphere/imports/text/ImportTextMain.java
@@ -0,0 +1,19 @@
+package edu.cmu.pslc.learnsphere.imports.text;
+
+import edu.cmu.pslc.datashop.workflows.AbstractComponent;
+
+public class ImportTextMain extends AbstractComponent {
+
+    public static void main(String[] args) {
+
+            ImportTextMain tool = new ImportTextMain();
+        tool.startComponent(args);
+    }
+
+    public ImportTextMain() {
+        super();
+
+    }
+
+
+}
