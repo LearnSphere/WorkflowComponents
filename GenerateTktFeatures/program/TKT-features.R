8dcf95aae9cf82602848f50ae3ae477a4b87dd46
ppavl <ppavl@Neptune>
2017-07-06 10:57:04 -0500

    Updated generate TKT

--- GenerateTktFeatures/program/TKT-features.R
+++ GenerateTktFeatures/program/TKT-features.R
@@ -4,19 +4,86 @@ ech<-FALSE
 # Read script parameters
 args <- commandArgs(trailingOnly = TRUE)
 
+
 #load library
 library(caTools)
 
+# initialize variables
+inputFile = NULL
+KCmodelsuper = NULL
+KCmodelsub = NULL
+workingDirectory = NULL
+componentDirectory = NULL
+flags = NULL
+
+# parse commandline args
+i = 1
+while (i <= length(args)) {
+    if (args[i] == "-file0") {
+       if (length(args) == i) {
+          stop("input file name must be specified")
+       }
+       inputFile = args[i+1]
+       i = i+1
+    } else if (args[i] == "-model") {
+       if (length(args) == i) {
+          stop("model name must be specified")
+       }
+       KCmodelsuper <- gsub("[ ()-]", ".", args[i+1])
+       i = i+1
+    } else if (args[i] == "-modelsub") {
+       if (length(args) == i) {
+          stop("model name must be specified")
+       }
+       KCmodelsub <- gsub("[ ()-]", ".", args[i+1])
+       i = i+1
+    } else if (args[i] == "-workingDir") {
+       if (length(args) == i) {
+          stop("workingDir name must be specified")
+       }
+       # This dir is the working dir for the component instantiation.
+       workingDirectory = args[i+1]
+       i = i+1
+    } else if (args[i] == "-programDir") {
+       if (length(args) == i) {
+          stop("programDir name must be specified")
+       }
 # This dir is the root dir of the component code.
-componentDirectory = args[2]
-# This dir is the working dir for the component instantiation.
-workingDirectory = args[4]
+       componentDirectory = args[i+1]
+       i = i+1
+    } 
+    i = i+1
+}
+
+clean <- file(paste(workingDirectory, "TKT-features-log.wfl", sep=""))
+sink(clean,append=TRUE)
+sink(clean,append=TRUE,type="message") # get error reports also
+options(width=120)
+
+if (is.null(inputFile) || is.null(KCmodelsub) || is.null(KCmodelsuper)|| is.null(workingDirectory) || is.null(componentDirectory) ) {
+   if (is.null(inputFile)) {
+      warning("Missing required input parameter: -file0")
+   }
+   if (is.null(KCmodelsub)) {
+      warning("Missing required input parameter: -modelsub")
+   } 
+   if (is.null(KCmodelsuper)) {
+      warning("Missing required input parameter: -model")
+   }
+   if (is.null(workingDirectory)) {
+      warning("Missing required input parameter: -workingDir")
+   }
+   if (is.null(componentDirectory)) {
+      warning("Missing required input parameter: -programDir")
+   }
+
+
+   stop("Usage: -programDir component_directory -workingDir output_directory -file0 input_file -model kc_model")
+}
+
 # This dir contains the R program or any R helper scripts
 programLocation<- paste(componentDirectory, "/program/", sep="")
 
-KCmodelsuper <- gsub("[ ()-]", ".", args[8])
-KCmodelsub <- gsub("[ ()-]", ".", args[10])
-inputFile<-args[12]
 outputFilePath<- paste(workingDirectory, "transaction file with added features.txt", sep="")
 
 # Get data
@@ -24,10 +91,6 @@ datalocation<- paste(componentDirectory, "/program/", sep="")
 val<-read.table(inputFile,sep="\t", header=TRUE,na.strings="",quote="",comment.char = "")
 
 # Creates output log file (use .wfl extension if you want the file to be treated as a logging file and hide from user)
-clean <- file(paste(workingDirectory, "TKT-features-log.wfl", sep=""))
-sink(clean,append=TRUE)
-sink(clean,append=TRUE,type="message") # get error reports also
-options(width=120)
 
 # Feature functions
 corcount <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))           #counts correct for index
