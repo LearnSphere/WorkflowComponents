8f4504c6c67a7cab50474b3aed9f66bec7b1ee87
mkomisin <mkomisin@LAPTOP-MANH4EUT>
2017-09-26 09:16:01 -0400

    Fix R install_dependencies.R info

--- AnalysisRglm/program/install_dependencies.R
+++ AnalysisRglm/program/install_dependencies.R
@@ -1,6 +1,5 @@
-# iAFM R package dependencies (to be run in R console as Administrator or sudo)
+# AnalysisRglm package dependencies (to be run in R console as Administrator or sudo)
 
-options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
+install.packages('lme4', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')
 
-install.packages("lme4", lib="/usr/local/lib/R/3.3/site-library")
-
+or    sudo su - -c "R -e \"install.packages('lme4', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')\""
