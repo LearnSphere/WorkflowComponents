69439a3c5410ecbe0e06abbfb3749b2b6380eace
ppavl <ppavl@Neptune>
2017-10-01 15:44:00 -0500

    Main changes for Memphis components for Oct 2017 release

--- GenerateTktFeatures/program/TKT-features.R
+++ GenerateTktFeatures/program/TKT-features.R
@@ -19,40 +19,40 @@ flags = NULL
 # parse commandline args
 i = 1
 while (i <= length(args)) {
-    if (args[i] == "-file0") {
-       if (length(args) == i) {
-          stop("input file name must be specified")
-       }
-       inputFile = args[i+1]
-       i = i+1
-    } else if (args[i] == "-model") {
-       if (length(args) == i) {
-          stop("model name must be specified")
-       }
-       KCmodelsuper <- gsub("[ ()-]", ".", args[i+1])
-       i = i+1
-    } else if (args[i] == "-modelsub") {
-       if (length(args) == i) {
-          stop("model name must be specified")
-       }
-       KCmodelsub <- gsub("[ ()-]", ".", args[i+1])
-       i = i+1
-    } else if (args[i] == "-workingDir") {
-       if (length(args) == i) {
-          stop("workingDir name must be specified")
-       }
-       # This dir is the working dir for the component instantiation.
-       workingDirectory = args[i+1]
-       i = i+1
-    } else if (args[i] == "-programDir") {
-       if (length(args) == i) {
-          stop("programDir name must be specified")
-       }
-# This dir is the root dir of the component code.
-       componentDirectory = args[i+1]
-       i = i+1
-    } 
+  if (args[i] == "-file0") {
+    if (length(args) == i) {
+      stop("input file name must be specified")
+    }
+    inputFile = args[i+1]
     i = i+1
+  } else if (args[i] == "-model") {
+    if (length(args) == i) {
+      stop("model name must be specified")
+    }
+    KCmodelsuper <- gsub("[ ()-]", ".", args[i+1])
+    i = i+1
+  } else if (args[i] == "-modelsub") {
+    if (length(args) == i) {
+      stop("model name must be specified")
+    }
+    KCmodelsub <- gsub("[ ()-]", ".", args[i+1])
+    i = i+1
+  } else if (args[i] == "-workingDir") {
+    if (length(args) == i) {
+      stop("workingDir name must be specified")
+    }
+    # This dir is the working dir for the component instantiation.
+    workingDirectory = args[i+1]
+    i = i+1
+  } else if (args[i] == "-programDir") {
+    if (length(args) == i) {
+      stop("programDir name must be specified")
+    }
+    # This dir is the root dir of the component code.
+    componentDirectory = args[i+1]
+    i = i+1
+  } 
+  i = i+1
 }
 
 clean <- file(paste(workingDirectory, "TKT-features-log.wfl", sep=""))
@@ -61,24 +61,24 @@ sink(clean,append=TRUE,type="message") # get error reports also
 options(width=120)
 
 if (is.null(inputFile) || is.null(KCmodelsub) || is.null(KCmodelsuper)|| is.null(workingDirectory) || is.null(componentDirectory) ) {
-   if (is.null(inputFile)) {
-      warning("Missing required input parameter: -file0")
-   }
-   if (is.null(KCmodelsub)) {
-      warning("Missing required input parameter: -modelsub")
-   } 
-   if (is.null(KCmodelsuper)) {
-      warning("Missing required input parameter: -model")
-   }
-   if (is.null(workingDirectory)) {
-      warning("Missing required input parameter: -workingDir")
-   }
-   if (is.null(componentDirectory)) {
-      warning("Missing required input parameter: -programDir")
-   }
-
-
-   stop("Usage: -programDir component_directory -workingDir output_directory -file0 input_file -model kc_model")
+  if (is.null(inputFile)) {
+    warning("Missing required input parameter: -file0")
+  }
+  if (is.null(KCmodelsub)) {
+    warning("Missing required input parameter: -modelsub")
+  } 
+  if (is.null(KCmodelsuper)) {
+    warning("Missing required input parameter: -model")
+  }
+  if (is.null(workingDirectory)) {
+    warning("Missing required input parameter: -workingDir")
+  }
+  if (is.null(componentDirectory)) {
+    warning("Missing required input parameter: -programDir")
+  }
+  
+  
+  stop("Usage: -programDir component_directory -workingDir output_directory -file0 input_file -model kc_model")
 }
 
 # This dir contains the R program or any R helper scripts
@@ -94,75 +94,75 @@ val<-read.table(inputFile,sep="\t", header=TRUE,na.strings="",quote="",comment.c
 
 # Feature functions
 corcount <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))           #counts correct for index
-                               for (i in unique(index)){
-                                 temp[index==i]<-
-                                   c(0,cumsum(df$CF..ansbin.[index==i]==1)
-                                     [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
-                               return(temp)}
+for (i in unique(index)){
+  temp[index==i]<-
+    c(0,cumsum(df$CF..ansbin.[index==i]==1)
+      [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
+return(temp)}
 
 incorcount <-function(df, index) {temp<-rep(0,length(df$CF..ansbin.))        #counts incorrect for index
-                                  for (i in unique(index)){
-                                    temp[index==i]<-
-                                      c(0,cumsum(df$CF..ansbin.[index==i]==0)
-                                        [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
-                                  return(temp)}
+for (i in unique(index)){
+  temp[index==i]<-
+    c(0,cumsum(df$CF..ansbin.[index==i]==0)
+      [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
+return(temp)}
 
 studycount <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))         #counts studies for index
-                                 for (i in unique(index)){
-                                   temp[index==i]<-
-                                     c(0,cumsum(df$CF..ansbin.[index==i]==-1)
-                                       [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
-                                 return(temp)}
+for (i in unique(index)){
+  temp[index==i]<-
+    c(0,cumsum(df$CF..ansbin.[index==i]==-1)
+      [1:(length(cumsum(df$CF..ansbin.[index==i ]))-1)])}
+return(temp)}
 
 KCspacing <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))          #computes spacing from prior repetition for index (in seconds)
-                                for (i in unique(index)){
-                                  lv<-length(df$CF..Time.[index==i])
-                                  temp[index==i]<-
-                                    c(0,df$CF..Time.[index==i][2:(lv)] - df$CF..Time.[index==i][1:(lv-1)])}
-                                return(temp)}
+for (i in unique(index)){
+  lv<-length(df$CF..Time.[index==i])
+  temp[index==i]<-
+    c(0,df$CF..Time.[index==i][2:(lv)] - df$CF..Time.[index==i][1:(lv-1)])}
+return(temp)}
 
 KCage <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))              #adds spacings to compute age since first trial (in seconds)
-                            for (i in unique(index)){
-                              temp[index==i]<-pmax(cumsum(df$CF..KCclusterspacing.[index==i])-df$Duration..sec.[index==i]/2,1)}
-                            return(temp)}
+for (i in unique(index)){
+  temp[index==i]<-pmax(cumsum(df$CF..KCclusterspacing.[index==i])-df$Duration..sec.[index==i][1]/2,1)}
+return(temp)}
 
 meanspacing <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))    #computes mean spacing
 for (i in unique(index)){
   j<-length(temp[index==i])
-if(j>1){temp[index==i][2]<-1}
-if(j==3){temp[index==i][3]<-max(df$CF..KCclusterspacing.[index==i][2]-(df$Duration..sec.[index==i][2:(j-1)])/2,1)}
+  if(j>1){temp[index==i][2]<-1}
+  if(j==3){temp[index==i][3]<-max(df$CF..KCclusterspacing.[index==i][2]-(df$Duration..sec.[index==i][1]/2),1)}
   if(j>3){temp[index==i][3:j]<-pmax(runmean(df$CF..KCclusterspacing.[index==i][2:(j-1)],
-                                      k=25,alg=c("exact"),align=c("right"))-(df$Duration..sec.[index==i][2:(j-1)])/2,1)}}
+                                            k=25,alg=c("exact"),align=c("right"))-(df$Duration..sec.[index==i][1])/(2*(j-2)),1)}}
+return(temp)}
+
+
+practiceTime <-function(df) {   temp<-rep(0,length(df$CF..ansbin.))
+for (i in unique(df$Anon.Student.Id)){
+  temp[df$Anon.Student.Id==i]<-
+    c(0,cumsum(df$Duration..sec.[df$Anon.Student.Id==i])
+      [1:(length(cumsum(df$Duration..sec.[df$Anon.Student.Id==i]))-1)])}
+return(temp)}
+
+KCintspacing <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))          #computes spacing from prior repetition for index (in seconds)
+for (i in unique(index)){
+  lv<-length(df$CF..Time.[index==i])
+  temp[index==i]<-
+    c(0,df$CF..practiceTime.[index==i][2:(lv)] - df$CF..practiceTime.[index==i][1:(lv-1)])}
+return(temp)}
+
+KCageint <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))              #adds spacings to compute age since first trial (in seconds)
+for (i in unique(index)){
+  temp[index==i]<-pmax(cumsum(df$CF..KCintspacing.[index==i])-df$Duration..sec.[index==i][1]/2,1)}
 return(temp)}
 
 meanspacingint <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))    #computes mean spacing
 for (i in unique(index)){
   j<-length(temp[index==i])
   if(j>1){temp[index==i][2]<-1}
-if(j==3){temp[index==i][3]<-max(df$CF..KCintspacing.[index==i][2]-(df$Duration..sec.[index==i][2:(j-1)])/2,1)}
+  if(j==3){temp[index==i][3]<-max(df$CF..KCintspacing.[index==i][2]-(df$Duration..sec.[index==i][1]/2),1)}
   if(j>3){temp[index==i][3:j]<-pmax(runmean(df$CF..KCintspacing.[index==i][2:(j-1)],
-                                      k=25,alg=c("exact"),align=c("right"))-(df$Duration..sec.[index==i][2:(j-1)])/2,1)}}
+                                            k=25,alg=c("exact"),align=c("right"))-(df$Duration..sec.[index==i][1])/(2*(j-2)),1)}}
 return(temp)}
-
-practiceTime <-function(df) {   temp<-rep(0,length(df$CF..ansbin.))
-                                for (i in unique(df$Anon.Student.Id)){
-                                   temp[df$Anon.Student.Id==i]<-
-                                     c(0,cumsum(df$Duration..sec.[df$Anon.Student.Id==i])
-                                       [1:(length(cumsum(df$Duration..sec.[df$Anon.Student.Id==i]))-1)])}
-                                 return(temp)}
-
-KCintspacing <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))          #computes spacing from prior repetition for index (in seconds)
-                                for (i in unique(index)){
-                                  lv<-length(df$CF..Time.[index==i])
-                                  temp[index==i]<-
-                                    c(0,df$CF..practiceTime.[index==i][2:(lv)] - df$CF..practiceTime.[index==i][1:(lv-1)])}
-                                return(temp)}
-
-KCageint <-function(df,index) {temp<-rep(0,length(df$CF..ansbin.))              #adds spacings to compute age since first trial (in seconds)
-                            for (i in unique(index)){
-                              temp[index==i]<-pmax(cumsum(df$CF..KCintspacing.[index==i])-df$Duration..sec.[index==i]/2,1)}
-                            return(temp)}
-
 
 #Feature creation
 val$CF..ansbin.<-ifelse(tolower(val$Outcome)=="correct",1,ifelse(tolower(val$Outcome)=="incorrect",0,-1))
