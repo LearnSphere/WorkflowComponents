9238f438c9596f1f7fe3d48e018a162882667b8e
Hui Cheng <hcheng@cs.cmu.edu>
2017-10-04 12:18:50 -0400

    add dependency instruction for TransformPivot python program

---
+++ TransformPivot/program/install_dependencies.R
@@ -0,0 +1,5 @@
+# TransformPivot package dependencies (to be run in R console as Administrator or sudo)
+
+install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')
+
+or    sudo su - -c "R -e \"install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')\""
