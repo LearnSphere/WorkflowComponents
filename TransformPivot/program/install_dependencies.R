7e8095366d1cd2d9bee8168bb30ebe9601bc8986
Hui Cheng <hcheng@cs.cmu.edu>
2017-10-05 15:48:09 -0400

    change install_dependencies.r and a few build.properties.sample

--- TransformPivot/program/install_dependencies.R
+++ TransformPivot/program/install_dependencies.R
@@ -1,5 +1,5 @@
 # TransformPivot package dependencies (to be run in R console as Administrator or sudo)
 
-install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')
+install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
 
-or    sudo su - -c "R -e \"install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R')\""
+or    sudo su - -c "R -e \"install.packages('reshape2', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
