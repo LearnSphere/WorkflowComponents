f5c51c8f71f77d1798853fa984b0e731a95b0e31
Hui Cheng <hcheng@cs.cmu.edu>
2017-10-12 12:30:13 -0400

    fix pip install command

--- MOOCdbFeatureExtract/program/install_dependencies.py
+++ MOOCdbFeatureExtract/program/install_dependencies.py
@@ -1,5 +1,7 @@
-# This component needs scipy.stats assuming MYSQL-python, phpserialize, sqlparse and numpy are installed already
+# This component needs scipy (version 0.18.1) assuming MYSQL-python, phpserialize, sqlparse and numpy are installed already
 #python version is 2.7
 #follwings are command to run on Unix to install required libraries
-sudo /usr/bin/pip2 install -U scipy=0.18.1
+#scipy version 0.18.1
+sudo /usr/bin/pip2 install -U scipy==0.18.1
+
 
