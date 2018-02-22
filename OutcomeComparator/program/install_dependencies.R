92a4f73bb3222a0e47e874ea0987dd236560c391
mkomisin <mkomisin@LAPTOP-MANH4EUT>
2018-02-22 14:44:55 -0500

    Outcome comparator: install_dependencies.R

---
+++ OutcomeComparator/program/install_dependencies.R
@@ -0,0 +1,4 @@
+# Outcome Comparator dependencies (to be run in R console as Administrator or sudo)
+
+options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
+install.packages("dplyr", lib="/usr/local/lib/R/3.3/site-library")
