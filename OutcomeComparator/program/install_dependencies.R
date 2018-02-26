04406ea09029c519bcbbb1830110dacbe7fab039
Hui Cheng <hcheng@cs.cmu.edu>
2018-02-26 15:42:21 -0500

    add tibble to dependencies file

--- OutcomeComparator/program/install_dependencies.R
+++ OutcomeComparator/program/install_dependencies.R
@@ -2,3 +2,7 @@
 
 options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
 install.packages("dplyr", lib="/usr/local/lib/R/3.3/site-library")
+
+options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
+install.packages("tibble", lib="/usr/local/lib/R/3.3/site-library")
+
