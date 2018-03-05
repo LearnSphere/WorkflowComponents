312dfd4142f43f9a6007756ca3d9a8481810e1e2
mkomisin <mkomisin@LAPTOP-MANH4EUT>
2018-03-05 11:00:43 -0500

    Updated Workflow Components.docx to cover handling of compressed input files.

--- UnzipTemplate/source/edu/cmu/pslc/learnsphere/analysis/UnzipTemplate/UnzipTemplate.java
+++ UnzipTemplate/source/edu/cmu/pslc/learnsphere/analysis/UnzipTemplate/UnzipTemplate.java
@@ -16,32 +16,36 @@ public class UnzipTemplate extends AbstractComponent {
         super();
     }
 
-    @Override
-    protected void runComponent() {
+@Override
+protected void runComponent() {
 
-    	Integer inNodeIndex = 0;
-    	Integer inFileIndex = 0;
-    	File inputFile = null;
-    	try {
-    		inputFile = this.getAttachmentAndUnzip(inNodeIndex, inFileIndex);
-    	} catch (Exception e) {
-    		System.err.println("Cannot unzip compressed input file.");
-    	}
+	Integer inNodeIndex = 0;
+	Integer inFileIndex = 0;
+	File zipOutputDirectory = null;
+	try {
+		zipOutputDirectory = this.getAttachmentAndUnzip(inNodeIndex, inFileIndex);
+	} catch (Exception e) {
+		System.err.println("Cannot unzip compressed input file.");
+	}
 
-    	if (inputFile != null) {
-    		File rCreatedFile = new File(inputFile.getAbsolutePath().replaceAll("\\\\", "/") + "/ds1_student_step_export.txt");
-	    	this.setInputFile(inNodeIndex, inFileIndex, rCreatedFile);
-	        // Run the program and return its stdout to a file.
-	        File outputDirectory = this.runExternal();
+	if (zipOutputDirectory != null) {
+		// Get the desired file path (relative)
+		String zipOutputPath = zipOutputDirectory.getAbsolutePath().replaceAll("\\\\", "/");
+		// In this example, we already know the realtive path to the desired file.
+		String desiredFile =  zipOutputPath + "/ds1_student_step_export.txt";
+		File rCreatedFile = new File(desiredFile);
+    	this.setInputFile(inNodeIndex, inFileIndex, rCreatedFile);
+        // Run the program and return its stdout to a file.
+        File outputDirectory = this.runExternal();
 
-	        Integer nodeIndex = 0;
-	        Integer fileIndex = 0;
-	        String fileLabel = "text";
-
-	        this.addOutputFile(new File(outputDirectory + "/my_output_file.txt"), nodeIndex, fileIndex, fileLabel);
-    	}
-        // Send the component output bakc to the workflow.
-        System.out.println(this.getOutput());
-    }
+        Integer nodeIndex = 0;
+        Integer fileIndex = 0;
+        String fileLabel = "text";
+        // Add the new file (created by analysis.R) to the output file list.
+        this.addOutputFile(new File(outputDirectory + "/my_output_file.txt"), nodeIndex, fileIndex, fileLabel);
+	}
+    // Send the component output back to the workflow.
+    System.out.println(this.getOutput());
+}
 
 }
