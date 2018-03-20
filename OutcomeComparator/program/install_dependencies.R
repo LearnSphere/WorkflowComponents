e44f9f03e6995ea8a1e35979c8133761140ca6f3
Hui Cheng <hcheng@cs.cmu.edu>
2018-03-20 11:47:19 -0400

    delete outcomeComparator; added new outputComparator to replace in the previous commit

--- OutcomeComparator/program/install_dependencies.R
+++
@@ -1,6 +0,0 @@
-# Outcome Comparator dependencies (to be run in R console as Administrator or sudo)
-
-options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
-install.packages("dplyr", lib="/usr/local/lib/R/3.3/site-library")
-install.packages("tibble", lib="/usr/local/lib/R/3.3/site-library")
-
