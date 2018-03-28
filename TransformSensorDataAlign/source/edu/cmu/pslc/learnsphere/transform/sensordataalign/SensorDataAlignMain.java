9cd6141c0205895985eddcf2e2db590d94f45c8d
ppavl <ppvlik@memphis.edu>
2018-03-28 10:38:30 -0500

    SensorData component bug fix

--- TransformSensorDataAlign/source/edu/cmu/pslc/learnsphere/transform/sensordataalign/SensorDataAlignMain.java
+++ TransformSensorDataAlign/source/edu/cmu/pslc/learnsphere/transform/sensordataalign/SensorDataAlignMain.java
@@ -1,10 +1,13 @@
 package edu.cmu.pslc.learnsphere.transform.sensordataalign;
 
 import java.io.File;
+
+import edu.cmu.pslc.datashop.workflows.AbstractComponent;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import edu.cmu.pslc.datashop.workflows.AbstractComponent;
+
 
 public class SensorDataAlignMain extends AbstractComponent {
 
@@ -55,7 +58,7 @@ public class SensorDataAlignMain extends AbstractComponent {
     
     @Override
     protected void runComponent() {  
-        File outputDirectory = this.runExternal();
+        File outputDirectory = this.runExternalMultipleFileOuput();
         //String dir= this.getToolPath();
     
         //.. Attach the output files to the component output with addOutputFile...//
@@ -82,3 +85,70 @@ public class SensorDataAlignMain extends AbstractComponent {
 
 
 }
+
+
+/*
+package edu.cmu.pslc.learnsphere.generate.pfafeatures;
+
+import java.io.File;
+
+import edu.cmu.pslc.datashop.workflows.AbstractComponent;
+
+public class PFAMain extends AbstractComponent {
+
+    /** Component option (model). */
+/*
+    String modelName = null;
+
+    public static void main(String[] args) {
+
+        PFAMain tool = new PFAMain();
+        tool.startComponent(args);
+    }
+
+    public PFAMain() {
+        super();
+    }
+
+
+    @Override
+    protected void parseOptions() {
+
+        if (this.getOptionAsString("model") != null) {
+            modelName = this.getOptionAsString("model").replaceAll("(?i)\\s*KC\\s*\\((.*)\\)\\s*", "$1");
+        }
+
+    }
+
+    @Override
+    protected void processOptions() {
+        logger.info("Processing Options");
+        // Add the column headers from our input file to this component's output metadata,
+        // plus one extra column for Predicted Error Rate, if it doesn't already exist.
+
+        // addMetaDataFromInput(String fileType, Integer inputNodeIndex, Integer outputNodeIndex, String name)
+        this.addMetaDataFromInput("transaction", 0, 0, ".*");
+        this.addMetaData("transaction", 0, META_DATA_LABEL, "label0", 0, "KC (" + modelName + ")");
+    }
+
+    @Override
+    protected void runComponent() {
+        // Run the program and add the files it generates to the component output.
+        File outputDirectory = this.runExternalMultipleFileOuput();
+        // Attach the output files to the component output: file_type = "analysis-summary", label = ""
+        if (outputDirectory.isDirectory() && outputDirectory.canRead()) {
+            File outputFile = new File(outputDirectory.getAbsoluteFile() + "/transaction file with added features.txt");
+            Integer nodeIndex0 = 0;
+            Integer fileIndex0 = 0;
+            String label0 = "transaction";
+            logger.info("Added file: " + outputFile.getAbsolutePath());
+            this.addOutputFile(outputFile, nodeIndex0, fileIndex0, label0);
+        }
+
+        // Send the component output back to the workflow.
+        System.out.println(this.getOutput());
+    }
+}
+
+
+*/
