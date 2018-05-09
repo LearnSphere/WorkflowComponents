8caeecf58aeb928ccc99c783353ccb293eeab2d3
Liang Zhang <lzhang13@memphis.edu>
2018-05-08 21:48:20 -0500

    ImportXAPI-1.1,Update ImportXAPImain.java and add info.xml.

--- ImportXAPI/source/edu/cmu/pslc/learnsphere/imports/xAPI/ImportXAPImain.java
+++ ImportXAPI/source/edu/cmu/pslc/learnsphere/imports/xAPI/ImportXAPImain.java
@@ -12,29 +12,18 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
 
 import gov.adlnet.xapi.client.StatementClient;
-import gov.adlnet.xapi.model.Activity;
-import gov.adlnet.xapi.model.ActivityDefinition;
 import gov.adlnet.xapi.model.Actor;
 import gov.adlnet.xapi.model.Agent;
-import gov.adlnet.xapi.model.InteractionComponent;
-import gov.adlnet.xapi.model.Statement;
 import gov.adlnet.xapi.model.StatementResult;
-import gov.adlnet.xapi.model.Verb;
-import gov.adlnet.xapi.model.Verbs;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
-import org.apache.commons.lang.StringUtils;
-import org.json.JSONObject;
 
 public class ImportXAPImain extends AbstractComponent {
 
@@ -85,52 +74,31 @@ public class ImportXAPImain extends AbstractComponent {
 	        Integer nodeIndex = 0;
                 Integer fileIndex = 0;
                 String fileType = "text";
-
 	    }
  
 	    public void getXAPIdata(String url,String username,String password,String filter,String customfilter,String filterValue) throws Exception {
                 
 	    	StatementClient client = new StatementClient(url, username, password);
 	    	String jsonTxt =null;
-                StatementResult results =null;
-                Statement statement = new Statement();
+                StatementResult results = null;
+                client = getStatementClientWithFilter(filter,filterValue, client,customfilter);
+                results = client.getStatements();
                 
-	        switch (filter) {
-	        case "Null":
-	        	break;
-	        case "filterByVerb":
-	        	results=client.filterByVerb(filterValue).getStatements();
-	        	break;
-	        case "filterByActor":
-	        	Actor actor = new Agent();
-	        	actor.setName(filterValue);
-	        	results=client.filterByActor(actor).getStatements();
-	        case "filterByActivity":
-	        	results=client.filterByActivity(filterValue).getStatements();
-	        	break;
-	        case "filterByRegistration":
-	        	results=client.filterByRegistration(filterValue).getStatements();
-	        	break;
-	        case "filterBySince":
-	        	results=client.filterBySince(filterValue).getStatements();
-	        	break;
-	        case "filterByUntil":
-	        	results=client.filterByUntil(filterValue).getStatements();
-	        	break;
-	        case "Custom":
-	        	client.addFilter(customfilter,filterValue);
-	        	break;
-	        default:
-	            this.addErrorMessage("Invalid filter type");
-	        }
-           
 	    	// Retrieving xAPI statements
 	        try {
-//		    	StatementResult results = client.getStatements();
-				//StatementResult nextPage = client.getStatements(results.getMore());  
-				Object object= results.getStatements();
-				Gson gson = new Gson();
-				jsonTxt= gson.toJson(object);
+                     StringBuilder sb = new StringBuilder();
+                     Object object= results.getStatements();
+                     Gson gson = new Gson();
+	             sb.append(gson.toJson(object));
+                     while(results.hasMore()){
+                        String moreString = results.getMore();
+                        moreString = moreString.replace("/data/xAPI", "");
+                        results = client.getStatements(moreString);
+                        Object obj = results.getStatements();
+                        sb.append(gson.toJson(object));
+                     }  
+			
+                     jsonTxt= gson.toJson(object);
 	        } catch (Exception e) {
 	            // TODO Auto-generated catch block
 	            e.printStackTrace();
@@ -174,10 +142,17 @@ public class ImportXAPImain extends AbstractComponent {
                 TabTextWriter writer = new TabTextWriter();
 	        List<Map<String, String>> flatJson = parser.parseJson(jsonTxt);
                
+                File generatedFile_0 = this.createFile("xAPI-JsonFlattener-file", ".txt");
+                FileWriter oStream_0 = new FileWriter(generatedFile_0);
+	        BufferedWriter sw_0 = new BufferedWriter(oStream_0);
+	        sw_0.write(writer.writeAsTxt(flatJson));
+                
+                
                int rows=array.length;
                int columns=array[0].length;
                List<String> items = new ArrayList<String>();
                
+               //Write headers as list
                Set<String> headers = collectHeaders(flatJson);
                List mainKeys= new ArrayList();
                int count=0; 
@@ -186,46 +161,93 @@ public class ImportXAPImain extends AbstractComponent {
                     mainKeys.add(index);
                 }
                
-               for (int row=0;row<rows;row++){
-                   for (int column=0;column<columns;column++){
-                       if(mainKeys.contains(array[row][column])){
-                           mainKeys.remove(array[row][column]);
+               //Headers: List to array
+               String[] tabNames=new String[mainKeys.size()];
+               for (int i=0; i<mainKeys.size();i++){
+                   tabNames[i]=(String) mainKeys.get(i);
+               }
+
+               //Values filled into array matrix 
+               List<String> mainValueList = new ArrayList<String>();
+               String [][] mainContent=new String[tabNames.length][];
+               for (int k=0;k<tabNames.length;k++){
+                    mainValueList.clear();
+                    for (Map<String, String> map: flatJson){
+                       String mainValue=map.get(tabNames[k]);
+                       mainValueList.add(mainValue); 
+                    } 
+                    
+                    Object[] mainValueArr= mainValueList.toArray();
+                    String mainValueArrStr[]=new String[mainValueArr.length];
+                    System.arraycopy(mainValueArr, 0, mainValueArrStr,0, mainValueArr.length);
+                    mainContent[k]=mainValueArrStr;
+               }
+               
+               //remove the "-" symbol of id
+               for(int k=0;k<tabNames.length;k++){
+                   if(tabNames[k].equals("id")){
+                       for(int rs=0;rs<mainContent[0].length;rs++){
+                           mainContent[k][rs]=mainContent[k][rs].replace("-","");
                        }
                    }
                }
                
-                 File generatedFile_0 = this.createFile("xAPI-JsonFlattener-file", ".txt");
-                 FileWriter oStream_0 = new FileWriter(generatedFile_0);
-	         BufferedWriter sw_0 = new BufferedWriter(oStream_0);
-	         sw_0.write(writer.writeAsTxt(flatJson));
-                 
-               int length=mainKeys.size();
-               for (int k=0;k<mainKeys.size();k++){
-                   for (Map<String, String> map: flatJson){
-                       if (map.containsKey(mainKeys.get(k))){
-                            map.remove(mainKeys.get(k));
-                            for (int row=0;row<rows;row++){
-                                String key=array[row][0];
-                                String value=map.get(array[row][1]);
-                                map.put(key, value);
-                            }
+               //Transfer Time Format
+               for(int k=0;k<tabNames.length;k++){
+                   if(tabNames[k].equals("stored")){
+                       for(int rs=0;rs<mainContent[0].length;rs++){
+                           SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                           Date date = dt.parse(mainContent[k][rs]);
+                           SimpleDateFormat dt1 = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+                           mainContent[k][rs]=dt1.format(date);
+                       }
+                   }
+               }
+             
+               for(int k=0;k<tabNames.length;k++){
+                   if(tabNames[k].equals("timestamp")){
+                       for(int rs=0;rs<mainContent[0].length;rs++){
+                           SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+                           Date date = dt.parse(mainContent[k][rs]);
+                           SimpleDateFormat dt1 = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+                           mainContent[k][rs]=dt1.format(date);
+                       }
+                   }
+               }               
+               
+               //Create array matrix about selected columns
+               String [][] selectContent=new String[array.length][];
+               for(int sc=0;sc<array.length;sc++){
+                   for (int k=0;k<tabNames.length;k++){
+                       if(tabNames[k].equals(array[sc][1])){
+                       selectContent[sc]=mainContent[k];
+                       }
+                   }
+               }
+                
+               //output the combination of tabNames and selectContents
+               
+               	File generatedFile = this.createFile("xAPI-TabDelimited-file", ".txt");
+                try {
+                    if (!generatedFile.exists()) {
+                         generatedFile.createNewFile();
+                    }
+                    FileWriter fw = new FileWriter(generatedFile.getAbsoluteFile());
+                    try (BufferedWriter bw = new BufferedWriter(fw)) {
+//                    bw.write(key, 0, key.length());
+                        for (String[] array1 : array) {
+                            bw.write(array1[0]+"\t");
+                        }
+                        for (int rs=0;rs<selectContent[0].length;rs++){
+                            bw.newLine();
+                            for (String[] array2:selectContent){
+                            bw.write(array2[rs]+"\t");
+                           }
                         }
                     }
-               }
-               
-               
-               for (int r=0;r<rows;r++){
-                   for (Map<String, String> map: flatJson){
-                       map.remove(array[r][1]);
-                   }  
-               }
-               
-
-	        File generatedFile = this.createFile("xAPI-TabDelimited-file", ".txt");
-	        FileWriter oStream = new FileWriter(generatedFile);
-	        BufferedWriter sw = new BufferedWriter(oStream);
-	        sw.write(writer.writeAsTxt(flatJson));
-
+                } catch (IOException e) {
+                }
+                
 	    Integer nodeIndex = 0;
             Integer fileIndex = 0;
             String fileType = "text";
@@ -240,4 +262,42 @@ public class ImportXAPImain extends AbstractComponent {
             return headers;
             }
 
+            private StatementClient getStatementClientWithFilter(String filter,String filterValue, StatementClient client,String customfilter){
+                StatementClient outputClient = null;
+                try{
+                    switch (filter) {
+                        case "Null":
+                                break;
+                        case "filterByVerb":
+                                outputClient=client.filterByVerb(filterValue);
+                                break;
+                        case "filterByActor":
+                                Actor actor = new Agent(null,filterValue);
+                                outputClient=client.filterByActor(actor);
+                                break;
+                        case "filterByActivity":
+                                outputClient=client.filterByActivity(filterValue);
+                                break;
+                        case "filterByRegistration":
+                                outputClient=client.filterByRegistration(filterValue);
+                                break;
+                        case "filterBySince":
+                                outputClient=client.filterBySince(filterValue);
+                                break;
+                        case "filterByUntil":
+                                outputClient=client.filterByUntil(filterValue);
+                                break;
+                        case "Custom":
+                                outputClient = client.addFilter(customfilter,filterValue);
+                                break;
+                        default:
+                            this.addErrorMessage("Invalid filter type");
+                    }
+                    return outputClient;
+                }catch(Exception e){
+                    logger.fatal(e.toString());
+                    return null;
+                }
+            }            
+            
 }
