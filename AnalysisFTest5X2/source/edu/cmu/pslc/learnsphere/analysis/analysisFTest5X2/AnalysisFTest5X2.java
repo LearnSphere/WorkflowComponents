ac5ae6024127010de9554ce69192653bf60c1300
Phil Pavlik <ppvlik@memphis.edu>
2018-05-23 12:06:32 -0500

    Fix 5x2 output code

--- AnalysisFTest5X2/source/edu/cmu/pslc/learnsphere/analysis/analysisFTest5X2/AnalysisFTest5X2.java
+++ AnalysisFTest5X2/source/edu/cmu/pslc/learnsphere/analysis/analysisFTest5X2/AnalysisFTest5X2.java
@@ -21,42 +21,42 @@ public class AnalysisFTest5X2 extends AbstractComponent {
 
     @Override
     protected void parseOptions() {
-	
 
-     
+
+
     }
 
     @Override
     protected void processOptions() {
- 
+
     }
 
     @Override
     protected void runComponent() {
-        // Run the program...
-       // File outputDirectory = this.runExternal();
-        File outputDirectory = this.runExternal();
-        Integer nodeIndex = 0;
-        Integer fileIndex = 0;
-        String fileLabel = "inline-html";
-        
-        Integer nodeIndex1 = 1;
-        Integer fileIndex1 = 0;
-        String fileLabel1 = "text";
-        System.out.println(outputDirectory.getAbsolutePath() + "\\test.html");
-        File studentStepFile = new File(outputDirectory.getAbsolutePath() + "/ test.html");
-        System.out.println( studentStepFile.exists());
+
+      File outputDirectory = this.runExternal();
+      if (outputDirectory.isDirectory() && outputDirectory.canRead()) {
+
+        File file0 = new File(outputDirectory.getAbsolutePath() + "/test.html");
         File file1 = new File(outputDirectory.getAbsolutePath() + "/R_output_model_summary.txt");
-        this.addOutputFile(studentStepFile, nodeIndex, fileIndex, fileLabel);
-        this.addOutputFile(file1, nodeIndex1, fileIndex1, fileLabel1);
-/*
-	nodeIndex = 1;
-        fileLabel = "model-values";
-        File valuesFile = new File(outputDirectory.getAbsolutePath() + "/test.html");
-        this.addOutputFile(valuesFile, nodeIndex, fileIndex, fileLabel);
-*/
-	
-        // Send the component output back to the workflow.
-        System.out.println(this.getOutput());
+        if (file0 != null && file0.exists() && file1 != null && file1.exists()) {
+
+          Integer nodeIndex = 0;
+          Integer fileIndex = 0;
+          String fileLabel = "inline-html";
+          this.addOutputFile(file0, nodeIndex, fileIndex, fileLabel);
+
+          Integer nodeIndex1 = 1;
+          Integer fileIndex1 = 0;
+          String fileLabel1 = "text";
+          this.addOutputFile(file1, nodeIndex1, fileIndex1, fileLabel1);
+
+        } else {
+          this.addErrorMessage("Files missing.");
+        }
+  this.addErrorMessage("Directory missing.");
+      }
+      // Send the component output back to the workflow.
+      System.out.println(this.getOutput());
     }
 }
