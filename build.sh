79dbf0fa865fa83d2ca5f42ee9ab914be49121fd
mkomisin <mkomisin@LAPTOP-MANH4EUT>
2018-06-07 11:02:42 -0400

    Improved build.sh script, now stores errors in build_errors.txt and relevant compilation info for broken builds in build_errors_info.txt.

--- build.sh
+++ build.sh
@@ -20,14 +20,30 @@ for cdir in `find $dir -maxdepth 1  -type d -name "[^.]*"`; do
   fi
   cd $cdir
 
-  if [ -f build.xml ]; then
-    ant dist
+  if [ -f build.xml ] && [ "$cdir" != "$dir" ]; then
+    buildOutput=`ant dist 2>> ${dir}/build_errors.txt`
+    if [ "$?" != 0 ]; then
+      echo ""
+      echo "******************************************"
+      echo "Error building jar in ${cdir}"
+      echo "******************************************"
+      echo ""
+      echo ${buildOutput} >> build_errors_info.txt
+    else
+      echo "Success: ${cdir}"
+    fi
   fi
 
   cd $dir
 done
 
 rm -Rf */test/ComponentTestOutput */WorkflowComponent.log */build
+
+if [ ! -s diff ${dir}/build_errors.txt ]; then
+   echo "Errors were found during the build process. Please see build_errors.txt or build_errors_info.txt for additional details."
+else
+   rm ${dir}/build_errors.txt ${dir}/build_errors_info.txt
+fi
 
 
 
