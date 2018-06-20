26e44679f53f8b266a17d5f0462f127946611819
Steven C Dang <stevencdang@cmu.edu>
2018-06-20 16:50:39 +0000

    Adding DatasetImporter built on docker container

---
+++ DatasetImporter/gen_add_component.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Ensure this is pointing to where WorkflowComponents is cloned
+wcc=$WCC 
+
+out_file="add_component.sql"
+comp_dir_name="DatasetImporter"
+
+# Remove add_component.sql if it exists
+if [ -f $out_file ]; then
+    rm $out_file
+fi
+
+echo "REPLACE INTO \`workflow_component\` (" >> $out_file
+echo -e "\t\`component_type\`," >> $out_file
+echo -e "\t\`component_name\`," >> $out_file
+echo -e "\t\`tool_dir\`," >> $out_file
+echo -e "\t\`schema_path\`," >> $out_file
+echo -e "\t\`interpreter_path\`," >> $out_file
+echo -e "\t\`tool_path\`," >> $out_file
+echo -e "\t\`enabled\`," >> $out_file
+echo -e "\t\`author\`," >> $out_file
+echo -e "\t\`citation\`," >> $out_file
+echo -e "\t\`version\`," >> $out_file
+echo -e "\t\`info\`" >> $out_file
+echo -e ")" >> $out_file
+echo "VALUES (" >> $out_file
+echo -e "\t'Analysis'," >> $out_file
+echo -e "\t'Dataset_Importer'," >> $out_file
+echo -e "\t'$wcc/$comp_dir_name/'," >> $out_file
+echo -e "\t'$wcc/$comp_dir_name/schemas/$comp_dir_name""_v1_0.xsd'," >> $out_file
+echo -e "\t'/usr/bin/java -jar'," >> $out_file
+echo -e "\t'$wcc/$comp_dir_name/dist/$comp_dir_name-1.0.jar', " >> $out_file
+echo -e "\t1," >> $out_file
+echo -e "\t'system'," >> $out_file
+echo -e "\t'Steven_C_Dang'," >> $out_file 
+echo -e "\t'1.0', " >> $out_file
+echo -e "\t'Make a dataset repository available within a workflow to select datasets'" >> $out_file
+echo -e ");" >> $out_file
---
+++ DatasetImporter/program/main.py
@@ -0,0 +1,65 @@
+
+# Author: Steven C. Dang
+
+# Main script for importing provided D3M dataset schemas for operation on datasets
+
+import logging
+import os.path as path
+import os
+import csv
+
+# Workflow component specific imports
+from ls_utilities.ls_logging import setup_logging
+from ls_utilities.cmd_parser import get_default_arg_parser
+from ls_utilities.ls_wf_settings import *
+from ls_dataset.d3m_dataset import D3MDataset
+
+__version__ = '0.1'
+
+if __name__ == '__main__':
+
+    # Parse argumennts
+    parser = get_default_arg_parser("Import List of available D3M Datasets")
+    args = parser.parse_args()
+    
+    if args.is_test is not None:
+        is_test = args.is_test == 1
+    else:
+        is_test = False
+
+    # Get config file
+    config = SettingsFactory.get_settings(path.join(args.programDir, 'program', 'settings.cfg'), 
+                                          program_dir=args.programDir,
+                                          working_dir=args.workingDir,
+                                          is_test=is_test
+                                          )
+
+    # Setup Logging
+    setup_logging(config)
+    logger = logging.getLogger('datasets_importer')
+
+    ### Begin Script ###
+    logger.info("Importing List of available datasets")
+    logger.debug("Running Dataset Importer with arguments: %s" % str(args))
+
+    # Read in the dataset json
+    ds_root = config.get_dataset_path()
+    datasets = set()
+    for root, dirs, files in os.walk(ds_root):
+        for f in files:
+            if f == 'datasetDoc.json':
+                logger.debug("Found dataset in directory: %s" % root)
+                ds = D3MDataset.from_dataset_json(path.join(root, f))
+                if ds.name not in datasets:
+                    logger.info("Found dataset name: %s\nAt path: %s" % (ds.name,  ds.dpath))
+                    datasets.add(ds.name)
+
+    logger.debug("Found datasets: %s" % str(datasets))
+
+
+    # # Write dataset info to output file
+    out_file_path = path.join(args.workingDir, config.get('Dataset', 'out_file'))
+    logger.info("Writing dataset list of %i datasets to file: %s" % (len(datasets), out_file_path))
+    with open(out_file_path, 'w') as out_file:
+        out_csv = csv.writer(out_file, delimiter='\t')
+        out_csv.writerow(datasets)
