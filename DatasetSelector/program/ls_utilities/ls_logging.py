b76762e1422757b2b6fd62945a1382b4c6bab6d0
Steven C Dang <stevencdang@cmu.edu>
2018-06-20 16:53:13 +0000

    adding Dataset Selector component with modified build.xml

---
+++ DatasetSelector/program/ls_utilities/ls_logging.py
@@ -0,0 +1,101 @@
+
+# Author: Steven C. Dang
+
+# Convenience class and functions for supporting logging in Workflow components
+
+import logging
+from logging.handlers import SysLogHandler
+from logging import StreamHandler
+from logging import FileHandler
+from datetime import datetime as dt
+import os.path as path
+import os
+import sys
+
+# logger = logging.getLogger('ls_logging')
+
+def setup_logging(settings):
+    """
+    Basic logging configuration. Takes 3 parameters:
+
+    Inputs
+        logger - a logger given from logging.getLogger()
+        config - a dictionary that expects 4 values:
+            log_level: {TRACE, DEBUG, INFO, WARNING, ERROR}
+            enable_syslog = True/False enable logging to local syslog
+            enable_file_log = True/False enable writing logs to file
+            file_log_path = file path string of where to write a log file
+        is_test - True/False flag to support logging to file when testing 
+            outside a workflow component.
+    Output: returns the given logger with appropriate adjustments
+
+    """
+    # Set the workingDir to current Directory if none is given
+    if settings.working_dir is None:
+        # logger.warning("Not working directory given. Using cwd")
+        workingDir = os.getcwd()
+    else:
+        workingDir = settings.working_dir
+   
+
+    # setup set of handlers to initiate logging config
+    lgr = logging.getLogger()
+
+    # Setup Logging to file in working directory
+    lgr.setLevel(settings.get_log_level())
+
+    # Set log msg format
+    formatter = logging.Formatter('%(levelname)s\t%(name)s\t%(asctime)s\t: %(message)s')
+    
+    # Write log msgs to sysLog if logging is enabled
+    if settings.is_syslog_enabled():
+        ch = SysLogHandler()
+        ch.setLevel(settings.get_log_level())
+        ch.setFormatter(formatter)
+        lgr.addHandler(ch)
+
+    # Write log msgs to file if enabled
+    if settings.is_file_log_enabled():
+        log_id = dt.now().isoformat()
+        if settings.get_file_log_path() is None:
+            log_file = path.join(workingDir, 'log-%s.txt' % log_id)
+        else:
+            log_file = path.join(settings.get_file_log_path(), 'log-%s.txt' % log_id)
+        ch = FileHandler(filename=log_file)
+        ch.setLevel(settings.get_log_level())
+        ch.setFormatter(formatter)
+        lgr.addHandler(ch)
+
+    # Add extra logging to file
+    if settings.is_test:
+        # logger.debug("not running as a tigris component")
+        # Add explicit file log
+        log_file = path.join(workingDir, 'test.log')
+        # logger.debug("Writing logs to : %s" % str(log_file))
+        ch = FileHandler(filename=log_file)
+        ch.setLevel(settings.get_log_level())
+        ch.setFormatter(formatter)
+        lgr.addHandler(ch)
+
+        # Also add logging to stdout when testing
+        ch = StreamHandler(sys.stdout)
+        ch.setLevel(settings.get_log_level())
+        ch.setFormatter(formatter)
+        lgr.addHandler(ch)
+
+
+    # Create stream handler to output error messages to stderr
+    ch = StreamHandler(sys.stderr)
+    ch.setLevel(logging.ERROR)
+    ch.setFormatter(formatter)
+    lgr.addHandler(ch)
+
+    # logging.basicConfig(handlers=handlers, level=config['log_level'])
+   
+    # Create separator in log file to make it easier to parse
+    for i in range(3):
+        lgr.info("####################################################")
+
+    return lgr
+
+
---
+++ DatasetSelector/program/main.py
@@ -0,0 +1,114 @@
+
+# Author: Steven C. Dang
+
+# Main script for importing provided D3M dataset schemas for operation on datasets
+
+import logging
+import os.path as path
+import sys
+import json
+import pprint
+import argparse
+import csv
+
+# Workflow component specific imports
+from ls_utilities.ls_logging import setup_logging
+from ls_utilities.cmd_parser import get_default_arg_parser
+from ls_utilities.ls_wf_settings import *
+from ls_dataset.d3m_dataset import D3MDataset
+
+__version__ = '0.1'
+
+if __name__ == '__main__':
+
+    # Parse argumennts
+    parser = get_default_arg_parser("Select Dataset")
+    parser.add_argument('-ds_name', type=str,
+                       help='the name of the dataset to import')
+    parser.add_argument('-file0', type=argparse.FileType('r'),
+                       help='the file list of all datasets')
+    args = parser.parse_args()
+
+    if args.is_test is not None:
+        is_test = args.is_test == 1
+    else:
+        is_test = False
+
+    # Get config file
+    # Get config file
+    config = SettingsFactory.get_settings(path.join(args.programDir, 'program', 'settings.cfg'), 
+                                          program_dir=args.programDir,
+                                          working_dir=args.workingDir,
+                                          is_test=is_test
+                                          )
+
+    # Setup Logging
+    setup_logging(config)
+    logger = logging.getLogger('dataset_selector')
+
+    ### Begin Script ###
+    logger.info("Importing D3M Dataset selected by user")
+    logger.debug("Running Dataset Import with arguments: %s" % str(args))
+
+    # Get selected dataset name
+    if args.ds_name is not None:
+        ds_name = args.ds_name
+    else:
+        ds_name = "baseball"
+
+    if args.is_test is not None:
+        is_test = args.is_test == 1
+    else:
+        is_test = False
+
+    # Lookup path to dataset from input file and selected datset option
+    ds_reader = csv.reader(args.file0, delimiter='\t')
+    for row in ds_reader:
+        logger.debug("Line number: %i" % ds_reader.line_num)
+        if ds_reader.line_num == 1:
+            logger.debug("Reading first line of dataset file")
+            # Dataset names are the first row
+            ds_names = row
+        elif ds_reader.line_num == 2:
+            logger.debug("Reading second line of dataset file")
+            # Dataset paths are the second row
+            ds_paths = row
+    i = ds_names.index(ds_name)
+    logger.debug("Index of chosen dataset: %i" % i)
+    name = ds_names[i]
+    logger.info("Selected Dataset: %s" % name)
+
+    # Crawl dataset directory to find dataset json with matching name
+    ds_root = config.get_dataset_path()
+    names = set()
+    datasets = {}
+
+    for root, dirs, files in os.walk(ds_root):
+        for f in files:
+            if f == 'datasetDoc.json':
+                logger.debug("Found dataset in directory: %s" % root)
+                ds = D3MDataset.from_dataset_json(path.join(root, f))
+                if ds.name not in names:
+                    logger.info("Found dataset name: %s\nAt path: %s" % (ds.name,  ds.dpath))
+                    names.add(ds.name)
+                    datasets[ds.name] = ds
+
+    ds = datasets[name]
+
+    logger.info("Found dataset with name %s, id: %s\n json: %s" % (ds.name, ds.id, str(ds)))
+
+    # Read in the dataset json
+    # schema_path = D3MDataset.get_schema_path(ds)
+    # with open(schema_path, 'r') as spath:
+        # logger.debug("opening dataset schema at path: %s" % schema_path)
+        # ds_data = json.load(spath)
+    # logger.debug("using json: %s" % str(ds_data)) 
+    # ds = D3MDataset(ds_path, ds_data)
+    logger.debug("Got dataset: %s" % str(ds))
+
+    # Write dataset info to output file
+    out_file_path = path.join(args.workingDir, config.get('Dataset', 'out_file'))
+    logger.info("Writing dataset json to: %s" % out_file_path)
+    ds.to_component_out_file(out_file_path)
+    if args.is_test == 1:
+        ds.to_json_pretty(out_file_path + '.readable')
