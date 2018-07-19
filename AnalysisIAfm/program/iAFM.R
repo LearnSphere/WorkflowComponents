4a0bf617643c6bfe97550b49714ad7fdfc553fa4
Hui Cheng <hcheng@cs.cmu.edu>
2018-07-19 10:07:38 -0400

    add xml output for iAFM component

--- AnalysisIAfm/program/iAFM.R
+++ AnalysisIAfm/program/iAFM.R
@@ -64,26 +64,50 @@ df <- preprocess(data.frame(read.table(file=stuStepFileName,na.string="NA",sep="
 ## fit iAFM - four params - student intercept, student slope, KC intercept, and KC slope
 iafm.model <- suppressWarnings(glmer(Success ~ Opportunity + (Opportunity|Anon.Student.Id) + (Opportunity|KC), data=df, family=binomial()))
 
-outputFile1 <- paste(workingDir, "/model-values.txt", sep="")
-
+outputFile1 <- paste(workingDir, "/model-values.xml", sep="")
+write("<model_values>",file=outputFile1,sep="",append=FALSE)
+write("<model>",file=outputFile1,sep="",append=TRUE)
+write(paste("<name>",modelName,"</name>",sep=""),file=outputFile1,sep="",append=TRUE)
 ## potential outputs
-write(paste("AIC",AIC(iafm.model),sep="\t"),file=outputFile1,sep="",append=FALSE)
-write(paste("BIC",BIC(iafm.model),sep="\t"),file=outputFile1,sep="",append=TRUE)
-write(paste("Log Likelihood",as.numeric(logLik(iafm.model)),sep="\t"),file=outputFile1,sep="",append=TRUE)
-write(paste("MAIN EFFECT intercept",fixef(iafm.model)[[1]],sep="\t"),file=outputFile1,sep="",append=TRUE)
-write(paste("MAIN EFFECT slope",fixef(iafm.model)[[2]],sep="\t"),file=outputFile1,sep="",append=TRUE)
-outputFile2 <- paste(workingDir, "/parameters.txt", sep="")
+write(paste("<AIC>",AIC(iafm.model),"</AIC>",sep=""),file=outputFile1,sep="",append=TRUE)
+write(paste("<BIC>",BIC(iafm.model),"</BIC>",sep=""),file=outputFile1,sep="",append=TRUE)
+write(paste("<log_likelihood>",as.numeric(logLik(iafm.model)),"</log_likelihood>",sep=""),file=outputFile1,sep="",append=TRUE)
+write(paste("<main_effect_intercept>",fixef(iafm.model)[[1]],"</main_effect_intercept>",sep=""),file=outputFile1,sep="",append=TRUE)
+write(paste("<main_effect_slope>",fixef(iafm.model)[[2]],"</main_effect_slope>",sep=""),file=outputFile1,sep="",append=TRUE)
+write("</model>",file=outputFile1,sep="",append=TRUE)
+write("</model_values>",file=outputFile1,sep="",append=TRUE)
+
+
+outputFile2 <- paste(workingDir, "/parameters.xml", sep="")
+write("<parameters>",file=outputFile2,sep="",append=FALSE)
+# kc.params is a table where column 1 is the KC name, column 2 is the iAFM estimated KC intercept, and column 3 is the iAFM estimated KC slope
+kc.params <- data.frame( cbind(row.names(ranef(iafm.model)$KC), ranef(iafm.model)$KC[,1], ranef(iafm.model)$KC[,2]) )
+kc.params <- cbind(Type="Skill", kc.params)
+for (x in 1:length(rownames(kc.params))) {
+  write("<parameter>",file=outputFile2,sep="",append=TRUE)
+  write("<type>Skill</type>",file=outputFile2,sep="",append=TRUE)
+  write(paste("<name>",kc.params[x,2],"</name>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write(paste("<intercept>",kc.params[x,3],"</intercept>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write(paste("<slope>",kc.params[x,4],"</slope>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write("</parameter>",file=outputFile2,sep="",append=TRUE)
+}
+#write.table(kc.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=FALSE,append=TRUE,row.names=FALSE)
 
 # stud.params is a table where column 1 is the student ID, column 2 is the iAFM estimated student intercept, and column 3 is the iAFM estimated student slope
 stud.params <- data.frame( cbind(row.names(ranef(iafm.model)$Anon.Student.Id), ranef(iafm.model)$Anon.Student.Id[,1], ranef(iafm.model)$Anon.Student.Id[,2]) )
 stud.params <- cbind(Type="Student", stud.params)
 colnames(stud.params) <- c("Type", "Name", "Intercept", "Slope")
-write.table(stud.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=TRUE,append=FALSE,row.names=FALSE)
+for (x in 1:length(rownames(stud.params))) {
+  write("<parameter>",file=outputFile2,sep="",append=TRUE)
+  write("<type>Student</type>",file=outputFile2,sep="",append=TRUE)
+  write(paste("<name>",stud.params[x,2],"</name>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write(paste("<intercept>",stud.params[x,3],"</intercept>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write(paste("<slope>",stud.params[x,4],"</slope>",sep=""),file=outputFile2,sep="",append=TRUE)
+  write("</parameter>",file=outputFile2,sep="",append=TRUE)
+}
+#write.table(stud.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=TRUE,append=FALSE,row.names=FALSE)
 
-# kc.params is a table where column 1 is the KC name, column 2 is the iAFM estimated KC intercept, and column 3 is the iAFM estimated KC slope
-kc.params <- data.frame( cbind(row.names(ranef(iafm.model)$KC), ranef(iafm.model)$KC[,1], ranef(iafm.model)$KC[,2]) )
-kc.params <- cbind(Type="Skill", kc.params)
-write.table(kc.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=FALSE,append=TRUE,row.names=FALSE)
+write("</parameters>",file=outputFile2,sep="",append=TRUE)
 
 # Prepare to write student-step file.
 outputFile3 <- paste(workingDir, "/student-step.txt", sep="")
