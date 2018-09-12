36814b7ad8fbeccb5558319ed67a6a90ed8ec5fd
Cindy Tipper <ctipper@cs.cmu.edu>
2018-09-12 12:05:29 -0400

    Update iAFM component with Paulo's performance improvements.

--- AnalysisIAfm/program/iAFM.R
+++ AnalysisIAfm/program/iAFM.R
@@ -2,67 +2,86 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 suppressMessages(library(lme4))
+suppressMessages(library(data.table))
+suppressMessages(library(optimx))
 
-preprocess <- function(origRollup, kcm) {
+preprocess <- function(origRollup, kcm,response,opportunity,individual) {
   #kcm_index <- grep(kcm,names(origRollup))
-  kcm_index <- which(names(origRollup)==kcm)
-  df <- origRollup[,c(3,5,7,15,kcm_index,kcm_index+1)]  # subset only the columns of interest
-  df$First.Attempt <- gsub("incorrect", 0, df$First.Attempt)  # convert correctness coding to binary, numeric
-  df$First.Attempt <- gsub("hint", 0, df$First.Attempt)
-  df$First.Attempt <- gsub("correct", 1, df$First.Attempt)
-  names(df) <- c("Anon.Student.Id","Problem.Name","Step.Name","Success","KC","Opportunity")  ## rename columns
-  df$Success <- as.numeric(as.vector(df$Success))  # convert success and opportunity columns to numeric
-  df$Opportunity <- as.numeric(as.vector(df$Opportunity)) - 1  # start opportunity count at 0
+  df = origRollup #the file to import
+  names(df) <- make.names(names(df)) #add the periods instead of spaces
+  names(df)[which( colnames(df)==make.names(eval(kcm)) )] <- "KC" #replace the KC model name with "KC"
+  names(df)[which( colnames(df)==make.names(eval(response)) )] <- "response" #replace the first attempt response name with "response"
+  names(df)[which( colnames(df)==make.names(eval(opportunity)) )] <- "opportunity" #replace the opportunity name with "opportunity"
+  names(df)[which( colnames(df)==make.names(eval(individual)) )] <- "individual" #replace the individualizing factor name with "individual"
+  success <- ifelse(df$response=="correct",1,0) #recode response as 0 (incorrect) or 1 (correct)
+  df$success <- success #add a success column
+  rm(success)
   return(df)
 }
 
 workingDir = "."
 
 if (length(args) == 2) {
-   stuStepFileName = args[1]
-   modelName = args[2]
+  stuStepFileName = args[1]
+  modelName = args[2]
 } else {
   i = 1
   while (i <= length(args)) {
-      if (args[i] == "-node") {
-       # Syntax follows: -node m -fileIndex n <infile>
-       if (i > length(args) - 4) {
-          stop("node and fileIndex must be specified")
-       }
-
-       nodeIndex <- args[i+1]
-       fileIndex = NULL
-       fileIndexParam <- args[i+2]
-       if (fileIndexParam == "-fileIndex") {
-           fileIndex <- args[i+3]
-       }
-
-       stuStepFileName <- args[i + 4]
-       i = i + 4
-
-    } else if (args[i] == "-model") {
-         if (length(args) == i) {
-            stop("model name must be specified")
-         }
-         modelName = args[i+1]
-         i = i+1
-      } else if (args[i] == "-workingDir") {
-         if (length(args) == i) {
-            stop("workingDir name must be specified")
-         }
-         workingDir = args[i+1]
-         i = i+1
+    if (args[i] == "-node") {
+      # Syntax follows: -node m -fileIndex n <infile>
+      if (i > length(args) - 4) {
+        stop("node and fileIndex must be specified")
       }
+      
+      nodeIndex <- args[i+1]
+      fileIndex = NULL
+      fileIndexParam <- args[i+2]
+      if (fileIndexParam == "-fileIndex") {
+        fileIndex <- args[i+3]
+      }
+      
+      stuStepFileName <- args[i + 4]
+      i = i + 4
+      
+    } else if (args[i] == "-model") {
+      if (length(args) == i) {
+        stop("model name must be specified")
+      }
+      modelName = args[i+1]
       i = i+1
+    } else if (args[i] == "-firstAttempt") {
+      if (length(args) == i) {
+        stop("model name must be specified")
+      }
+      response = args[i+1]
+      i = i+1
+    } else if (args[i] == "-opportunity") {
+      if (length(args) == i) {
+        stop("model name must be specified")
+      }
+      opportunity = args[i+1]
+      i = i+1
+    } else if (args[i] == "-student") {
+      if (length(args) == i) {
+        stop("model name must be specified")
+      }
+      individual = args[i+1]
+      i = i+1
+    } else if (args[i] == "-workingDir") {
+      if (length(args) == i) {
+        stop("workingDir name must be specified")
+      }
+      workingDir = args[i+1]
+      i = i+1
+    }
+    i = i+1
   }
 }
 
-## preprocess data -- customize the file path & kc model with workflow inputs
-df <- preprocess(data.frame(read.table(file=stuStepFileName,na.string="NA",sep="\t",quote="",header=T)),
-                 make.names(modelName))  ## make.names changes the inputted string to the same periods-based format that data.frame() does for column headers
+df <- preprocess(fread(file=stuStepFileName,verbose = F),eval(modelName),eval(response),eval(opportunity),eval(individual)) #i added eval() because we are passing the name of the columns to the preprocess function. this might not work depending on how the java is setup.
 
-## fit iAFM - four params - student intercept, student slope, KC intercept, and KC slope
-iafm.model <- suppressWarnings(glmer(Success ~ Opportunity + (Opportunity|Anon.Student.Id) + (Opportunity|KC), data=df, family=binomial()))
+## fit iAFM - four params - individual intercept, individual slope, KC intercept, and KC slope
+iafm.model <- suppressWarnings(glmer(success ~ opportunity + (opportunity|individual) + (opportunity|KC), data=df, family=binomial(),control = glmerControl(optimizer = "optimx", calc.derivs = FALSE,optCtrl = list(method = "nlminb", starttests = FALSE, kkt = FALSE))))
 
 outputFile1 <- paste(workingDir, "/model-values.xml", sep="")
 write("<model_values>",file=outputFile1,sep="",append=FALSE)
@@ -91,11 +110,10 @@ for (x in 1:length(rownames(kc.params))) {
   write(paste("<slope>",kc.params[x,4],"</slope>",sep=""),file=outputFile2,sep="",append=TRUE)
   write("</parameter>",file=outputFile2,sep="",append=TRUE)
 }
-#write.table(kc.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=FALSE,append=TRUE,row.names=FALSE)
 
 # stud.params is a table where column 1 is the student ID, column 2 is the iAFM estimated student intercept, and column 3 is the iAFM estimated student slope
-stud.params <- data.frame( cbind(row.names(ranef(iafm.model)$Anon.Student.Id), ranef(iafm.model)$Anon.Student.Id[,1], ranef(iafm.model)$Anon.Student.Id[,2]) )
-stud.params <- cbind(Type="Student", stud.params)
+stud.params <- data.frame( cbind(row.names(ranef(iafm.model)$individual), ranef(iafm.model)$individual[,1], ranef(iafm.model)$individual[,2]) )
+stud.params <- cbind(Type="Individual", stud.params)
 colnames(stud.params) <- c("Type", "Name", "Intercept", "Slope")
 for (x in 1:length(rownames(stud.params))) {
   write("<parameter>",file=outputFile2,sep="",append=TRUE)
@@ -105,7 +123,6 @@ for (x in 1:length(rownames(stud.params))) {
   write(paste("<slope>",stud.params[x,4],"</slope>",sep=""),file=outputFile2,sep="",append=TRUE)
   write("</parameter>",file=outputFile2,sep="",append=TRUE)
 }
-#write.table(stud.params,file=outputFile2,sep="\t",quote=FALSE,na="",col.names=TRUE,append=FALSE,row.names=FALSE)
 
 write("</parameters>",file=outputFile2,sep="",append=TRUE)
 
@@ -113,22 +130,20 @@ write("</parameters>",file=outputFile2,sep="",append=TRUE)
 outputFile3 <- paste(workingDir, "/student-step.txt", sep="")
 
 # Make note of original header, including column ordering
-origFile <- read.table(file=stuStepFileName,na.string="NA",sep="\t",quote="",header=T,check.names=FALSE)
+origFile <- fread(file=stuStepFileName,verbose = F)
 origCols <- colnames(origFile)
 
-# Remove the existing PER for the specified model
-perToDelete <- sub("KC ", "Predicted Error Rate ", modelName)
-modifiedFile <- within(origFile, rm(list=perToDelete))
+# Add PER for the specified model. if it exists replaces, if it doesn't exist gets added to the end
+actualModelName <- strsplit(modelName,split = '[()]')[[1]][length(strsplit(modelName,split = '[()]')[[1]])]
 
-# Add PER for the specified model... gets added to the end
-modifiedFile$PredictedErrorRate <- predict(iafm.model,df,type="response",allow.new.levels=TRUE)
+KCname = paste("KC (",eval(actualModelName),")",sep="")
+PERname = paste("Predicted Error Rate (",eval(actualModelName),")",sep="")
 
-# Rename the column
-colnames(modifiedFile)[colnames(modifiedFile)=="PredictedErrorRate"] <- perToDelete
-# Sort columns to match original file
-modifiedFile <- modifiedFile[, origCols]
+if(PERname%in%origCols){
+  origFile[,eval(PERname)] <- predict(iafm.model,df,type="response",allow.new.levels=TRUE) # replace the values in the column
+}else{
+  origFile$PredictedErrorRate <- predict(iafm.model,df,type="response",allow.new.levels=TRUE) # add the column
+  names(origFile)[ncol(origFile)] <- PERname # Rename the column
+}
 
-write.table(modifiedFile, file=outputFile3, sep="\t", quote=FALSE, na="", col.names=TRUE, append=FALSE, row.names=FALSE)
-
-
-
+fwrite(origFile, file=outputFile3,sep="\t")
