cc27dfd5f11e3458422843b9aa10088cfb3cab89
Liang Zhang <lzhang13@memphis.edu>
2018-10-24 16:02:02 -0500

    Adding new component GLRKT

--- GLRKT/source/edu/cmu/pslc/learnsphere/analysis/GLRKTMain.java
+++ GLRKT/source/edu/cmu/pslc/learnsphere/analysis/GLRKTMain.java
@@ -56,12 +56,29 @@ public class GLRKTMain extends AbstractComponent {
 	File outputDirectory = this.runExternal();
         
         if (outputDirectory.isDirectory() && outputDirectory.canRead()) {
-            File outputFile = new File(outputDirectory.getAbsoluteFile() + "/transaction file output.txt");
-            Integer nodeIndex0 = 0;
-            Integer fileIndex0 = 0;
-            String label0 = "tab-delimited";
-            logger.info("Added file: " + outputFile.getAbsolutePath());
-            this.addOutputFile(outputFile, nodeIndex0, fileIndex0, label0);
+            File file0 = new File(outputDirectory.getAbsolutePath() + "/transaction_file_output.txt");
+            File file1 = new File(outputDirectory.getAbsolutePath() + "/R_output_model_summary.txt");
+            File file2 = new File(outputDirectory.getAbsolutePath() + "/model_result_values.xml");
+            
+            if (file0 != null && file0.exists() && file1 != null && file1.exists()) {            
+                Integer nodeIndex0 = 0;
+                Integer fileIndex0 = 0;
+                String label0 = "transaction";
+                this.addOutputFile(file0, nodeIndex0, fileIndex0, label0);
+
+                Integer nodeIndex1 = 1;
+                Integer fileIndex1 = 0;
+                String label1 = "text";
+                this.addOutputFile(file1, nodeIndex1, fileIndex1, label1);
+
+                Integer nodeIndex2 = 2;
+                Integer fileIndex2 = 0;
+                String label2 = "text";
+                this.addOutputFile(file2, nodeIndex2, fileIndex2, label2);
+
+            }else{
+                this.addErrorMessage("An unknown error has occurred with the TKT component.");
+            }
         }
 
         System.out.println(this.getOutput());
