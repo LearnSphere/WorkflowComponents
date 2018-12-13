ea09f5d2b1bfe9204f4ba706da80954c2aae99b2
unknown <pls21@pitt.edu>
2018-12-13 17:50:55 -0500

    RISE: HTML output rather than PDF

--- RISE/source/edu/cmu/learnsphere/analysis/rise/RISEMain.java
+++ RISE/source/edu/cmu/learnsphere/analysis/rise/RISEMain.java
@@ -1,6 +1,15 @@
 package edu.cmu.learnsphere.analysis.rise;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import edu.cmu.pslc.datashop.workflows.AbstractComponent;
 
@@ -11,6 +20,7 @@ public class RISEMain extends AbstractComponent {
 
     /** Component option (generatePlot). */
     String generatePlot = null;
+    private static String htmlTemplateName = "riseTemplate.html";
 
     /**
      * Main method.
@@ -35,7 +45,7 @@ public class RISEMain extends AbstractComponent {
 
         // The addMetaData* methods make the meta data available to downstream components.
 	// Add additional meta-data for each output file.
-	this.addMetaData("pdf", 0, META_DATA_LABEL, "label0", 0, null);
+	this.addMetaData("inline-html", 0, META_DATA_LABEL, "label0", 0, null);
 
     }
 
@@ -58,12 +68,66 @@ public class RISEMain extends AbstractComponent {
 	File outputDirectory = this.runExternal();
 
 	File outputFile0 = new File(outputDirectory.getAbsolutePath() + "/rise.txt");
-	File outputFile1 = new File(outputDirectory.getAbsolutePath() + "/rise.pdf");
+
+	addDataReferenceToHtmlFile(outputFile0);
+	File outputFile1 = new File(outputDirectory.getAbsolutePath() + "/rise.html");
 
         this.addOutputFile(outputFile0, 0, 0, "tab-delimited");
-        this.addOutputFile(outputFile1, 1, 0, "pdf");
+        this.addOutputFile(outputFile1, 1, 0, "inline-html");
 
         System.out.println(this.getOutput());
 
+    }
+
+    private File addDataReferenceToHtmlFile(File riseData) {
+        File htmlTemplateFile = new File(this.getToolDir() + "/program/" + htmlTemplateName);
+        File outputFile = null;
+        if (htmlTemplateFile.exists() && htmlTemplateFile.isFile() && htmlTemplateFile.canRead()) {
+            outputFile = this.createFile("rise.html");
+
+            String outputSubpath = this.componentOutputDir
+                                   .replaceAll("\\\\", "/")
+                                   .replaceAll("^.*/workflows/", "workflows/");
+            String dataFilePath = "LearnSphere?htmlPath=" + outputSubpath + riseData.getName();
+
+            BufferedReader bReader = null;
+            FileReader fReader = null;
+
+            BufferedWriter bWriter = null;
+            FileWriter fWriter = null;
+
+            try {
+
+                fReader = new FileReader(htmlTemplateFile);
+                bReader = new BufferedReader(fReader);
+
+                fWriter = new FileWriter(outputFile);
+                bWriter = new BufferedWriter(fWriter);
+
+                String line = null;
+                while ((line = bReader.readLine()) != null) {
+                    if (line.contains("${input0}")) {
+                        line = line.replaceAll(Pattern.quote("${input0}"),
+                                               dataFilePath); // name is data.txt
+                    }
+
+                    bWriter.append(line + "\n");
+                }
+            } catch (IOException e) {
+                this.addErrorMessage(e.toString());
+            } finally {
+                try {
+                    if (bReader != null) {
+                        bReader.close();
+                    }
+                    if (bWriter != null) {
+                        bWriter.close();
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        }
+        return outputFile;
     }
 }
