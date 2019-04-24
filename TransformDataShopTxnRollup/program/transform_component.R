5dee0ba7c1a26069c9741462d77d94c8530a796a
Cindy Tipper <ctipper@cs.cmu.edu>
2019-04-24 13:43:10 -0400

    Fix units for determining time.

--- TransformDataShopTxnRollup/program/transform_component.R
+++ TransformDataShopTxnRollup/program/transform_component.R
@@ -81,12 +81,12 @@ rm(m,Levels)
 
 #summarize to get number of hours spent in tutor, number of problems completed, number of hits requested, number of errors,
 if (is.null(endDate)) {
-transform_data <- suppressWarnings(data[as.POSIXct(Time)>as.POSIXct(startDate),.(propCorrectSteps=length(Outcome[Outcome%in%c("OK")])/length(Outcome),hints=length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/3600,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`,newLevel)])
+transform_data <- suppressWarnings(data[as.POSIXct(Time)>as.POSIXct(startDate),.(propCorrectSteps=length(Outcome[Outcome%in%c("OK")])/length(Outcome),hints=length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/60,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`,newLevel)])
 } else {
-  transform_data <- suppressWarnings(data[as.POSIXct(Time)>as.POSIXct(startDate)&as.POSIXct(Time)<as.POSIXct(endDate),.(propCorrectSteps=length(Outcome[Outcome%in%c("OK")])/length(Outcome),hints=length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/3600,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`,newLevel)])
+  transform_data <- suppressWarnings(data[as.POSIXct(Time)>as.POSIXct(startDate)&as.POSIXct(Time)<as.POSIXct(endDate),.(propCorrectSteps=length(Outcome[Outcome%in%c("OK")])/length(Outcome),hints=length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/60,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`,newLevel)])
 }
 
-#transform_data <- suppressWarnings(data[,.(propCorrectSteps=1-(length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")])/length(Outcome)),hints=length(Outcome[Outcome=="ERROR"]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/3600,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`)]) #we might actually be able to get away without the Level summary but leaving it in for now.
+#transform_data <- suppressWarnings(data[,.(propCorrectSteps=1-(length(Outcome[Outcome%in%c("INITIAL_HINT","HINT_LEVEL_CHANGE")])/length(Outcome)),hints=length(Outcome[Outcome=="ERROR"]),time=sum(as.numeric(`Duration (sec)`),na.rm = T)/60,problems=length(unique(`Problem Name`)),steps=length(unique(`Step Name`)),date=as.character(max(as.POSIXct(Time)))),by=.(`Anon Student Id`)]) #we might actually be able to get away without the Level summary but leaving it in for now.
 
 
 #write the summarized data
