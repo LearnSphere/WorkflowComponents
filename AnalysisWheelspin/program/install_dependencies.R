c5f40331bd2f07cdcd7a36b85e75c894beed71ab
hcheng688 <hcheng@cs.cmu.edu>
2020-01-21 19:52:26 -0500

    add more dependent R packages

--- AnalysisWheelspin/program/install_dependencies.R
+++ AnalysisWheelspin/program/install_dependencies.R
@@ -1,8 +1,16 @@
-# AnalysisRglm package dependencies (to be run in R console as Administrator or sudo)
+# AnalysisWheelspin package dependencies (to be run in R console as Administrator or sudo)
 
 install.packages('lme4', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
+install.packages('tidyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
+install.packages('plyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
+install.packages('dplyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
+install.packages('arm', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')
 
 or    sudo su - -c "R -e \"install.packages('lme4', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
+or    sudo su - -c "R -e \"install.packages('tidyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
+or    sudo su - -c "R -e \"install.packages('plyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
+or    sudo su - -c "R -e \"install.packages('dplyr', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
+or    sudo su - -c "R -e \"install.packages('arm', repos = 'http://cran.cnr.berkeley.edu/',  lib='/usr/local/lib/R/library')\""
 
 #also install rlang, data.table, optimx, speedglm as above
 
