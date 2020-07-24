849d827e30182fff20dd10681346a36918309292
Paulo Carvalho <pcarvalh@andrew.cmu.edu>
2020-07-24 10:36:47 -0400

    two new components for PL2 data analyses
    Create by Arun Sharma :) email him for questions :)

---
+++ trendMAP/program/install_dependencies.R
@@ -0,0 +1,6 @@
+# iAFM R package dependencies (to be run in R console as Administrator or sudo)
+
+options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
+
+install.packages(c("tidyverse", lib="/usr/local/lib/R/3.3/site-library"))
+
