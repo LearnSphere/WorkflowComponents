32555ec2b1b734f73dcba6de31cf761802156439
aruns2 <arun.pmrdf@gmail.com>
2020-07-27 09:43:15 -0400

    removed description_pdf

--- description_pdf/program/install_dependencies.R
+++
@@ -1,7 +0,0 @@
-# iAFM R package dependencies (to be run in R console as Administrator or sudo)
-
-options(repos=structure(c(CRAN="http://cran.cnr.berkeley.edu/")))
-
-install.packages(c("tidyverse", lib="/usr/local/lib/R/3.3/site-library"))
-
-#also install logWarningsMessagesPkg_0.1.0.tar.gz which is located at CommonResources/packages/R/log_warnings_messages folder. ref README.md
