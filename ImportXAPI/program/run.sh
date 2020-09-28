503dc1210c20601003f17eaecf07b4c274c62f62
Liang Zhang <lzhang13@memphis.edu>
2020-09-28 00:11:56 -0500

    Update function for java 8,9/28/2020

--- ImportXAPI/program/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-"/usr/java/jdk1.8.0_131/jre/bin/java" -Djava.util.prefs.systemRoot=/usr/java/jdk1.8.0_131/jre/ -Djava.util.prefs.userRoot=/usr/java/jdk1.8.0_131/jre/ -cp /datashop/workflow_components/ImportXAPI/dist/ImportXAPI-1.0.jar -jar /datashop/workflow_components/ImportXAPI/dist/ImportXAPI-1.0.jar "$@"
