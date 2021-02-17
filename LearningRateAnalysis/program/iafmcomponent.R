e65583a64e6d69e3a89d034e793136d9a0cbd56b
mkomisin <mkomisin@cmu.edu>
2021-02-17 09:47:19 -0500

    trendMap and description_png components: suppress warning message about grouping by the dplyr change

--- LearningRateAnalysis/program/iafmcomponent.R
+++ LearningRateAnalysis/program/iafmcomponent.R
@@ -10,6 +10,9 @@
 #read arguments
 args <- commandArgs(trailingOnly = TRUE)
 
+# suppress warning message about grouping by the dplyr change
+options(dplyr.summarise.inform=F)
+
 #library
 suppressWarnings(suppressMessages(library(tidyverse)))
 suppressWarnings(suppressMessages(library(lmerTest)))
@@ -23,20 +26,20 @@ i = 1
 #parsing arguments
 inputFile = NULL
 while (i <= length(args)) {
-  
+
   if (args[i] == "-node") {
     # Syntax follows: -node m -fileIndex n <infile>
     if (i > length(args) - 4) {
       stop("node and fileIndex must be specified")
     }
-    
+
     nodeIndex <- args[i+1]
     fileIndex = NULL
     fileIndexParam <- args[i+2]
     if (fileIndexParam == "-fileIndex") {
       fileIndex <- args[i+3]
     }
-    
+
     if (nodeIndex == 0 && fileIndex == 0) {
       inputFile0 <- args[i+4]
     } else if (nodeIndex == 1 && fileIndex == 0) {
@@ -45,7 +48,7 @@ while (i <= length(args)) {
       inputFile2 <- args[i+4]
     }
     i = i+4
-    
+
   } else if (args[i] == "-workingDir") {
     if (length(args) == i) {
       stop("workingDir name must be specified")
@@ -86,48 +89,48 @@ transform.map <- function(dataset){
   if(is.data.frame(student) && ncol(student)==0){
     student <- data.frame(1:nrow(student))
   }
-  
+
   school <- dataset[,grepl("schoolname", tolower(names(dataset)))]
   if(is.data.frame(school) && ncol(school)==0){
     school <- data.frame(1:nrow(school))
   }
-  
+
   grade <- dataset[,grepl("grade", tolower(names(dataset)))]
   if(is.data.frame(grade) && ncol(grade)==0){
     grade <- data.frame(1:nrow(grade))
   }
-  
+
   gender <- dataset[,grepl("gender", tolower(names(dataset)))]
   if(is.data.frame(gender) && ncol(gender)==0){
     gender <- data.frame(1:nrow(gender))
   }
-  
+
   race <- dataset[,grepl("studentethnicgroup|race|ethnicity",
                          tolower(names(dataset)))]
   if(is.data.frame(race) && ncol(race)==0){
     race <- data.frame(1:nrow(race))
   }
-  
+
   termname <- dataset[,grepl("termname", tolower(names(dataset)))]
   if(is.data.frame(termname) && ncol(termname)==0){
     termname <- data.frame(1:nrow(termname))
   }
-  
+
   rit <- dataset[,grepl("testritscore", tolower(names(dataset)))]
   if(is.data.frame(rit) && ncol(rit)==0){
     rit <- data.frame(1:nrow(rit))
   }
-  
+
   discipline <- dataset[,grepl("discipline", tolower(names(dataset)))]
   if(is.data.frame(discipline) && ncol(discipline)==0){
     discipline  <- data.frame(1:nrow(discipline))
   }
-  
-  
+
+
   #combine
   dataset <- data.frame(student, school, grade, gender, race,
                         termname,rit,discipline)
-  
+
   names(dataset) <- c("student", "school", "grade", "gender", "race",
                       "termname","rit","discipline")
   return(dataset)
@@ -141,18 +144,18 @@ prepare.map <- function(dataset1, dataset2){
   # dataset2 <- data.1819
   data1 <- transform.map(dataset1)
   data2 <- transform.map(dataset2)
-  
+
   #only those students which are present in both years
   data1 <- data1[data1$student %in% data2$student,]
   data2 <- data2[data2$student %in% data1$student,]
   #remove empty terms without values
   data1 <- data1[data1$termname!= "",]
   data2 <- data2[data2$termname!= "",]
-  
-  #drop unused levels from 
+
+  #drop unused levels from
   data1$termname <- droplevels(data1$termname)
   data2$termname <- droplevels(data2$termname)
-  
+
   #checking for the year data to prepare factor
   orig.level1 <- levels(data1$termname)
   orig.level2 <- levels(data2$termname)
@@ -167,18 +170,18 @@ prepare.map <- function(dataset1, dataset2){
     data1[grepl("fall",tolower(data1$termname)), ]$term <- 1
     data1[grepl("winter",tolower(data1$termname)), ]$term <- 2
     data1[grepl("spring",tolower(data1$termname)), ]$term <- 3
-    
-    
+
+
     data2[grepl("fall",tolower(data2$termname)), ]$term <- 4
     data2[grepl("winter",tolower(data2$termname)), ]$term <- 5
     data2[grepl("spring",tolower(data2$termname)), ]$term <- 6
-    
-    data1$termname <- factor(data1$termname, 
+
+    data1$termname <- factor(data1$termname,
                              levels = c(currLevel1[grepl("fall",tolower(currLevel1))],
                                         currLevel1[grepl("winter", tolower(currLevel1))],
                                         currLevel1[grepl("spring", tolower(currLevel1))]))
-    
-    data2$termname <- factor(data2$termname, 
+
+    data2$termname <- factor(data2$termname,
                              levels = c(currLevel2[grepl("fall",tolower(currLevel2))],
                                         currLevel2[grepl("winter", tolower(currLevel2))],
                                         currLevel2[grepl("spring", tolower(currLevel2))]))
@@ -189,21 +192,21 @@ prepare.map <- function(dataset1, dataset2){
     data2[grepl("fall",tolower(data2$termname)), ]$term <- 1
     data2[grepl("winter",tolower(data2$termname)), ]$term <- 2
     data2[grepl("spring",tolower(data2$termname)), ]$term <- 3
-    
-    
-    data1$termname <- factor(data1$termname, 
+
+
+    data1$termname <- factor(data1$termname,
                              levels = c(currLevel1[grepl("fall",tolower(currLevel1))],
                                         currLevel1[grepl("winter", tolower(currLevel1))],
                                         currLevel1[grepl("spring", tolower(currLevel1))]))
-    
-    data2$termname <- factor(data2$termname, 
+
+    data2$termname <- factor(data2$termname,
                              levels = c(currLevel2[grepl("fall",tolower(currLevel2))],
                                         currLevel2[grepl("winter", tolower(currLevel2))],
                                         currLevel2[grepl("spring", tolower(currLevel2))]))
   }
-  
+
   dataset <- rbind(data1, data2)
-  return(dataset) 
+  return(dataset)
 }
 
 #iAFM and Plots for IAFM
@@ -214,107 +217,107 @@ plot.iafm <- function(data, choice){
   #model year 1
   data <- data.all[data.all$term %in%c(1,2,3),]
   model1 <- lmer(rit ~ term + (term|student), data= data)
-  
+
   stud.params <- data.frame( cbind(row.names(ranef(model1)$student), ranef(model1)$student[,1], ranef(model1)$student[,2]) )
   colnames(stud.params) <- c("student", "Intercept", "Slope")
   stud.params[,2] <- as.numeric(as.character(stud.params[,2]))
   stud.params[,3] <- as.numeric(as.character(stud.params[,3]))
-  
+
   table1 <- stud.params
   overall.intercept1 <- coef(summary(model1))[1]
   overall.slope1 <- coef(summary(model1))[2]
-  
+
   ## Analysing the difference between students before the intervention
-  
+
   final.data1 <- merge(data, stud.params, by = "student")
-  
+
   trial.data1 <- final.data1[final.data1$term == 1,]
-  trial.data1 <- trial.data1 %>% 
+  trial.data1 <- trial.data1 %>%
     mutate(id = row_number())
-  
+
   trial.data1$is.intervention <- as.factor(trial.data1$is.intervention)
   avg.slope.year1 <- mean(trial.data1[trial.data1$is.intervention==1,]$Slope)
   avg.incpt.year1 <- mean(trial.data1
                           [trial.data1$is.intervention==1,]$Intercept)
-  
+
   #model year 2
   data <- data.all[data.all$term %in%c(4,5,6),]
   data[data$term == 4,]$term <- 1
   data[data$term == 5,]$term <- 2
   data[data$term == 6,]$term <- 3
   model2 <- lmer(rit ~ term + (term|student), data= data)
-  
+
   stud.params <- data.frame( cbind(row.names(ranef(model2)$student), ranef(model2)$student[,1], ranef(model2)$student[,2]) )
   colnames(stud.params) <- c("student", "Intercept", "Slope")
   stud.params[,2] <- as.numeric(as.character(stud.params[,2]))
   stud.params[,3] <- as.numeric(as.character(stud.params[,3]))
-  
+
   table2 <- stud.params
   overall.intercept2 <- coef(summary(model2))[1]
   overall.slope2 <- coef(summary(model2))[2]
-  
-  
-  
+
+
+
   ## Analysing the difference between students before the intervention
-  
+
   final.data2 <- merge(data, stud.params, by = "student")
-  
+
   trial.data2 <- final.data2[final.data2$term == 3,]
-  trial.data2 <- trial.data2 %>% 
+  trial.data2 <- trial.data2 %>%
     mutate(id = row_number())
-  
+
   trial.data2$is.intervention <- as.factor(trial.data2$is.intervention)
   avg.slope.year2 <- mean(trial.data2[trial.data2$is.intervention==1,]$Slope)
   avg.incpt.year2 <- mean(trial.data2
                           [trial.data2$is.intervention==1,]$Intercept)
-  
+
   trial.data1$year <- 1
   trial.data2$year <- 2
-  
+
   trial.data <- rbind(trial.data1, trial.data2)
-  
+
   #trial.data <- trial.data[trial.data$is.intervention ==1,]
-  
-  
-  
+
+
+
   graph1 <- ggplot()+
     geom_abline(data = trial.data1[trial.data1$is.intervention ==1,],
-                aes(intercept = (Intercept+overall.intercept1), 
+                aes(intercept = (Intercept+overall.intercept1),
                     slope =(Slope+overall.slope1)),
                 alpha = 0.05 , color = "red",show.legend = TRUE)+
-    
+
     ylim(100,250)+
     geom_abline( mapping = aes(slope= avg.slope.year1+overall.slope1,
                                intercept = avg.incpt.year1+overall.intercept1
-    ), 
-    size = 2 , linetype = "dashed", color = "red", 
+    ),
+    size = 2 , linetype = "dashed", color = "red",
     show.legend  = TRUE)+
     geom_abline(data = trial.data2[trial.data2$is.intervention ==1,],
-                aes(intercept = (Intercept+overall.intercept2), 
+                aes(intercept = (Intercept+overall.intercept2),
                     slope =(Slope+overall.slope2)),
                 color = "darkgreen",alpha = 0.05,show.legend = TRUE)+
-    
+
     geom_abline( mapping = aes(slope= avg.slope.year2+overall.slope2,
                                intercept = avg.incpt.year2+overall.intercept2
     ), size = 2, color = "darkgreen",show.legend = TRUE )+
     scale_x_continuous(breaks= 1:3, labels = c("Fall" , "Winter ", "Spring"), limits = c(1,3) ) +labs(y = "MAP (RIT Score)", x = NULL)+
     ggtitle("iAFM: Score trend for both the years for STUDENTS WITH INTERVENTION", subtitle = "Year1 = RED, Year 2 = GREEN" )+ theme(axis.text.x = element_text(size = 12))
-  
+
   trial.data$newslope <-0
-  trial.data[trial.data$year == 1,]$newslope <- 
+  trial.data[trial.data$year == 1,]$newslope <-
     trial.data[trial.data$year == 1,]$Slope+overall.slope1
   trial.data[trial.data$year == 2,]$newslope <-
     trial.data[trial.data$year == 2,]$Slope+overall.slope2
-  
+
   table1 <- trial.data[,c("student", "Intercept", "Slope", "year")]
-  
+
   summary <- trial.data%>%
     group_by(is.intervention, year) %>%
     summarise( count = n(), avg.diff= mean(newslope), lower = t.test(newslope, mu =0)$conf.int[1], upper = t.test(newslope, mu =0)$conf.int[2] )
-  
-  
+
+
   #summary$app <- as.factor(as.character(summary$app))
-  
+
   graph2 <- summary %>%
     ggplot(aes( x= is.intervention, y= avg.diff, fill = is.intervention)) +
     geom_bar(stat = "identity") +
@@ -324,11 +327,11 @@ plot.iafm <- function(data, choice){
     theme(axis.text.x = element_text(angle = 60, hjust =1, vjust =1))+ labs(y = "Slope (Learning Rate)", x = NULL)+
     ggtitle("Slope(Learning Rate) difference", subtitle = "For those with intervention vs without intervention" )+
     facet_wrap(.~year)+scale_x_discrete(labels = NULL)+ scale_fill_discrete(labels = c("No", "Yes"))
-  
-  
+
+
   return(list(graph1,graph2, table1))
-  
-  
+
+
 }
 
 # code main
--- trendMAP_png/program/trendmap.R
+++ trendMAP_png/program/trendmap.R
@@ -8,6 +8,9 @@
 #read arguments
 args <- commandArgs(trailingOnly = TRUE)
 
+# suppress warning message about grouping by the dplyr change
+options(dplyr.summarise.inform=F)
+
 #library
 suppressWarnings(suppressMessages(library(tidyverse)))
 
@@ -19,27 +22,27 @@ i = 1
 #parsing arguments
 inputFile = NULL
 while (i <= length(args)) {
-  
+
     if (args[i] == "-node") {
       # Syntax follows: -node m -fileIndex n <infile>
       if (i > length(args) - 4) {
         stop("node and fileIndex must be specified")
       }
-      
+
       nodeIndex <- args[i+1]
       fileIndex = NULL
       fileIndexParam <- args[i+2]
       if (fileIndexParam == "-fileIndex") {
         fileIndex <- args[i+3]
       }
-      
+
       if (nodeIndex == 0 && fileIndex == 0) {
         inputFile0 <- args[i+4]
       } else if (nodeIndex == 1 && fileIndex == 0) {
         inputFile1 <- args[i+4]
       }
       i = i+4
-    
+
   } else if (args[i] == "-workingDir") {
     if (length(args) == i) {
       stop("workingDir name must be specified")
@@ -74,48 +77,48 @@ transform.map <- function(dataset){
   if(is.data.frame(student) && ncol(student)==0){
     student <- data.frame(1:nrow(student))
   }
-  
+
   school <- dataset[,grepl("schoolname", tolower(names(dataset)))]
   if(is.data.frame(school) && ncol(school)==0){
     school <- data.frame(1:nrow(school))
   }
-  
+
   grade <- dataset[,grepl("grade", tolower(names(dataset)))]
   if(is.data.frame(grade) && ncol(grade)==0){
     grade <- data.frame(1:nrow(grade))
   }
-  
+
   gender <- dataset[,grepl("gender", tolower(names(dataset)))]
   if(is.data.frame(gender) && ncol(gender)==0){
     gender <- data.frame(1:nrow(gender))
   }
-  
+
   race <- dataset[,grepl("studentethnicgroup|race|ethnicity",
                          tolower(names(dataset)))]
   if(is.data.frame(race) && ncol(race)==0){
     race <- data.frame(1:nrow(race))
   }
-  
+
   termname <- dataset[,grepl("termname", tolower(names(dataset)))]
   if(is.data.frame(termname) && ncol(termname)==0){
     termname <- data.frame(1:nrow(termname))
   }
-  
+
   rit <- dataset[,grepl("testritscore", tolower(names(dataset)))]
   if(is.data.frame(rit) && ncol(rit)==0){
     rit <- data.frame(1:nrow(rit))
   }
-  
+
   discipline <- dataset[,grepl("discipline", tolower(names(dataset)))]
   if(is.data.frame(discipline) && ncol(discipline)==0){
     discipline  <- data.frame(1:nrow(discipline))
   }
-  
-  
+
+
   #combine
   dataset <- data.frame(student, school, grade, gender, race,
                         termname,rit,discipline)
-  
+
   names(dataset) <- c("student", "school", "grade", "gender", "race",
                       "termname","rit","discipline")
   return(dataset)
@@ -128,7 +131,7 @@ trend.map <- function(dataset1, dataset2){
   #transforming dataset for analysis
   data1 <- transform.map(dataset1)
   data2 <- transform.map(dataset2)
-  
+
   #checking for the year data to prepare factor
   orig.level1 <- levels(data1$termname)
   orig.level2 <- levels(data2$termname)
@@ -143,18 +146,18 @@ trend.map <- function(dataset1, dataset2){
     data1[grepl("fall",tolower(data1$termname)), ]$term <- 1
     data1[grepl("winter",tolower(data1$termname)), ]$term <- 2
     data1[grepl("spring",tolower(data1$termname)), ]$term <- 3
-    
-    
+
+
     data2[grepl("fall",tolower(data2$termname)), ]$term <- 4
     data2[grepl("winter",tolower(data2$termname)), ]$term <- 5
     data2[grepl("spring",tolower(data2$termname)), ]$term <- 6
-    
-    data1$termname <- factor(data1$termname, 
+
+    data1$termname <- factor(data1$termname,
                              levels = c(currLevel1[grepl("fall",tolower(currLevel1))],
                                         currLevel1[grepl("winter", tolower(currLevel1))],
                                         currLevel1[grepl("spring", tolower(currLevel1))]))
-    
-    data2$termname <- factor(data2$termname, 
+
+    data2$termname <- factor(data2$termname,
                              levels = c(currLevel2[grepl("fall",tolower(currLevel2))],
                                         currLevel2[grepl("winter", tolower(currLevel2))],
                                         currLevel2[grepl("spring", tolower(currLevel2))]))
@@ -165,37 +168,37 @@ trend.map <- function(dataset1, dataset2){
     data2[grepl("fall",tolower(data2$termname)), ]$term <- 1
     data2[grepl("winter",tolower(data2$termname)), ]$term <- 2
     data2[grepl("spring",tolower(data2$termname)), ]$term <- 3
-    
-    
-    data1$termname <- factor(data1$termname, 
+
+
+    data1$termname <- factor(data1$termname,
                              levels = c(currLevel1[grepl("fall",tolower(currLevel1))],
                                         currLevel1[grepl("winter", tolower(currLevel1))],
                                         currLevel1[grepl("spring", tolower(currLevel1))]))
-    
-    data2$termname <- factor(data2$termname, 
+
+    data2$termname <- factor(data2$termname,
                              levels = c(currLevel2[grepl("fall",tolower(currLevel2))],
                                         currLevel2[grepl("winter", tolower(currLevel2))],
                                         currLevel2[grepl("spring", tolower(currLevel2))]))
     }
-  
+
   dataset <- rbind(data1, data2)
-  
-  
+
+
   #plot for mathematics as Minoritized
   dataset$is.minoritized <- "Minoritized"
   dataset[grepl("white|asian|w",tolower(dataset$race) ),]$is.minoritized <-
     "Non-Minoritized"
-  
+
   avg.race.maths <- dataset[dataset$discipline==subject,] %>%
-    group_by(is.minoritized, term) %>% 
+    group_by(is.minoritized, term) %>%
     summarise(avg.rit = mean(rit), count = n())
-  
+
   table1 <- avg.race.maths
-  
-  graph1 <- ggplot(dataset[dataset$discipline==subject,], 
-                   aes(termname, y = rit, color = is.minoritized)) + 
-    geom_line(aes(group = student), alpha = .1) + 
-    geom_line(data = avg.race.maths, aes(x = term, y = avg.rit, 
+
+  graph1 <- ggplot(dataset[dataset$discipline==subject,],
+                   aes(termname, y = rit, color = is.minoritized)) +
+    geom_line(aes(group = student), alpha = .1) +
+    geom_line(data = avg.race.maths, aes(x = term, y = avg.rit,
                                          color = is.minoritized),
               alpha = 1,size = 2)+
     theme_bw()+
