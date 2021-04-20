41abfaf08f44db752fec847a58691b53634784d5
mkomisin <mkomisin@cmu.edu>
2021-04-20 09:41:48 -0400

    CurriculumPacing: add option to suppress dplyr warning

--- CurriculumPacing/program/curriculumpacing.R
+++ CurriculumPacing/program/curriculumpacing.R
@@ -3,6 +3,7 @@
 suppressMessages(suppressWarnings(library(tibble)))
 suppressMessages(suppressWarnings(library(readr)))
 suppressMessages(suppressWarnings(library(ggplot2)))
+options(dplyr.summarise.inform = FALSE)
 
 override <- FALSE
 
@@ -55,11 +56,11 @@ graph_width = 12
 hierarchy_order <- NULL
 
 if (override) {
-  
+
   input_file <- "student_data.txt"
   working_dir <- "."
   program_dir <- "."
-  
+
   plot_type <- "Usage and performance"
   #plot_type <- "Usage"
   time_scale_type <- "Relative"
@@ -70,180 +71,180 @@ if (override) {
   abs_max_time_unit <- "3000-01-01 00:00:00"
   hierarchy_order <- "order_data.csv"
   #hierarchy_order <- NULL
-  
+
 } else {
-  
+
   # Read script parameters
   args <- commandArgs(trailingOnly = TRUE)
-  
+
   i <- 1
-  
+
   while (i <= length(args)) {
-    
+
     if (args[i] == "-node") {
       # input files for the component
       # syntax: -node m -fileIndex n <infile>
-      
+
       if (i > length(args) - 4) {
         stop("node and fileIndex must be specified")
       }
-      
+
       node_index <- args[i + 1]
       file_index <- NULL
       file_index_param <- args[i + 2]
-      
+
       if (file_index_param == "-fileIndex") {
         file_index <- args[i + 3]
       }
-      
+
       if (node_index == 0) {
         input_file <- args[i + 4]
         print(paste("Input file: ", input_file))
       }
-      
+
       if (node_index == 1) {
         hierarchy_order <- args[i + 4]
         print(paste("hierarchy_order file: ", hierarchy_order))
       }
-      
+
       i <- i + 4
-      
+
     } else if (args[i] == "-workingDir") {
       # working directory of the component
-      
+
       if (length(args) == i) {
         stop("workingDir name must be specified")
       }
-      
+
       working_dir <- args[i + 1]
       print(paste("Working dir:", working_dir))
       i <- i + 1
-      
+
     } else if (args[i] == "-programDir") {
       # program directory of the component
-      
+
       if (length(args) == i) {
         stop("programDir name must be specified")
       }
-      
+
       # This dir is the root dir of the component code.
       #program_dir <- paste(args[i + 1], "/program/", sep = "")
       program_dir <- args[i + 1]
       print(paste("Program dir:", program_dir))
       i <- i + 1
-      
+
     } else if (args[i] == "-Plot") {
       # option 1: plot_type
-      
+
       if (length(args) == i) {
         stop("Plot type must be specified")
       }
-      
+
       plot_type <- args[i + 1]
       print(paste("Plot type:", plot_type))
       i <- i + 1
-      
+
     } else if (args[i] == "-TimeScale") {
       # option 2: time_scale_type
-      
+
       if (length(args) == i) {
         stop("Time scale type must be specified")
       }
-      
+
       time_scale_type <- args[i + 1]
       print(paste("Time scale type:", time_scale_type))
       i <- i + 1
-      
+
     } else if (args[i] == "-TimeScaleRes") {
       # option 3: time_scale_res
-      
+
       if (length(args) == i) {
         stop("Time scale resolution must be specified")
       }
-      
+
       time_scale_res <- args[i + 1]
       print(paste("Time scale resolution:", time_scale_res))
       i <- i + 1
-      
+
     } else if (args[i] == "-RelMinTimeUnit") {
       # option 4: rel_min_time_unit
-      
+
       if (length(args) == i) {
         stop("Minimum relative time unit must be specified")
       }
-      
+
       rel_min_time_unit <- as.integer(args[i + 1])
       print(paste("Minimum relative time unit:", rel_min_time_unit))
       i <- i + 1
-      
+
     } else if (args[i] == "-RelMaxTimeUnit") {
       # option 5: rel_max_time_unit
-      
+
       if (length(args) == i) {
         stop("Maximum relative time unit must be specified")
       }
-      
+
       rel_max_time_unit <- as.integer(args[i + 1])
       print(paste("Maximum relative time unit:", rel_max_time_unit))
       i <- i + 1
-      
+
     } else if (args[i] == "-AbsMinTimeUnit") {
       # option 6: abs_min_time_unit
-      
+
       if (length(args) == i) {
         stop("Minimum absolute time unit must be specified")
       }
-      
+
       abs_min_time_unit <- args[i + 1]
       print(paste("Minimum absolute time unit:", abs_min_time_unit))
       i <- i + 1
-      
+
     } else if (args[i] == "-AbsMaxTimeUnit") {
       # option 7: abs_max_time_unit
-      
+
       if (length(args) == i) {
         stop("Maximum absolute time unit must be specified")
       }
-      
+
       abs_max_time_unit <- args[i + 1]
       print(paste("Maximum absolute time unit:", abs_max_time_unit))
       i <- i + 1
-      
+
     }  else if (args[i] == "-height") {
       # option 7: graph heigh
-      
+
       if (length(args) == i) {
         stop("Graph height must be specified")
       }
-      
+
       #graph_height <- as.numeric(args[i + 1])
       graph_height <- logWarningsMessages(as.numeric(args[i + 1]), logFileName = "curriculum_pacing.wfl")
-        
+
       print(paste("Graph height:", graph_height))
       i <- i + 1
-      
+
     } else if (args[i] == "-width") {
       # option 7: graph width
-      
+
       if (length(args) == i) {
         stop("Graph width must be specified")
       }
-      
+
       #graph_width <- as.numeric(args[i + 1])
       graph_width <- logWarningsMessages(as.numeric(args[i + 1]), logFileName = "curriculum_pacing.wfl")
       print(paste("Graph width:", graph_width))
       i <- i + 1
-      
-    } 
-    
-    
+
+    }
+
+
     i <- i + 1
   }
-  
+
 }
 
 #process RelMinTimeUnit, RelMaxTimeUnit, AbsMinTimeUnit, AbsMaxTimeUnit with time_scale_type relationship
-if (time_scale_type == "Relative") { 
+if (time_scale_type == "Relative") {
   if (is.null(rel_min_time_unit))
     stop("Minimum relative time unit must be specified")
   if (is.null(rel_max_time_unit))
@@ -276,7 +277,7 @@ plt <- make_plot(student_step_data = rdf,
                  time_scale_resolution = time_scale_res,
                  min_time_unit = rel_min_time_unit,
                  max_time_unit = rel_max_time_unit,
-                 min_datetime_unit = abs_min_time_unit, 
+                 min_datetime_unit = abs_min_time_unit,
                  max_datetime_unit = abs_max_time_unit,
                  plot_type = plot_type)
 
