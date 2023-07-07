1adeb5b5c64e9db4fb2ff6123e5be87f5f02f0aa
hcheng688 <hcheng@cs.cmu.edu>
2023-07-07 18:02:25 -0400

    add checking to iAFM so multiskill model isn't allowed

--- AnalysisIAfm/source/edu/cmu/pslc/learnsphere/analysis/AnalysisIAfm/AnalysisIAfm.java
+++ AnalysisIAfm/source/edu/cmu/pslc/learnsphere/analysis/AnalysisIAfm/AnalysisIAfm.java
@@ -3,6 +3,7 @@ package edu.cmu.pslc.learnsphere.analysis.AnalysisIAfm;
 import java.io.File;
 
 import edu.cmu.pslc.datashop.workflows.AbstractComponent;
+import edu.cmu.pslc.statisticalCorrectnessModeling.utils.IOUtil;
 
 public class AnalysisIAfm extends AbstractComponent {
 
@@ -39,26 +40,81 @@ public class AnalysisIAfm extends AbstractComponent {
 
     @Override
     protected void runComponent() {
-        // Run the program...
-        File outputDirectory = this.runExternal();
+    	//make sure opportunity is numeric otherwise the component will run a long time
+    	//this is to prevent multiskill model to run
+    	boolean reqsMet = true;
+    	File inputFile = getAttachment(0, 0);
+        logger.info("iAFM inputFile: " + inputFile.getAbsolutePath());
+        String opportunity = this.getOptionAsString("opportunity");
+        if (!isColumnNumeric(inputFile.getAbsolutePath(), opportunity)) {
+			String err = "Opportunity Column has non-numeric value.";
+            addErrorMessage(err);
+            logger.info("iAFM component is aborted: " + err);
+            reqsMet = false;
+		}
+        
+        if (reqsMet) {
+        	// Run the program...
+            File outputDirectory = this.runExternal();
 
-        Integer fileIndex = 0;
-        Integer nodeIndex = 0;
-        String fileLabel = "student-step";
-        File studentStepFile = new File(outputDirectory.getAbsolutePath() + "/student-step.txt");
-        this.addOutputFile(studentStepFile, nodeIndex, fileIndex, fileLabel);
+            Integer fileIndex = 0;
+            Integer nodeIndex = 0;
+            String fileLabel = "student-step";
+            File studentStepFile = new File(outputDirectory.getAbsolutePath() + "/student-step.txt");
+            this.addOutputFile(studentStepFile, nodeIndex, fileIndex, fileLabel);
 
-	nodeIndex = 1;
-        fileLabel = "model-values";
-        File valuesFile = new File(outputDirectory.getAbsolutePath() + "/model-values.xml");
-        this.addOutputFile(valuesFile, nodeIndex, fileIndex, fileLabel);
+    	nodeIndex = 1;
+            fileLabel = "model-values";
+            File valuesFile = new File(outputDirectory.getAbsolutePath() + "/model-values.xml");
+            this.addOutputFile(valuesFile, nodeIndex, fileIndex, fileLabel);
 
-	nodeIndex = 2;
-	fileLabel = "parameters";
-        File paramsFile = new File(outputDirectory.getAbsolutePath() + "/parameters.xml");
-        this.addOutputFile(paramsFile, nodeIndex, fileIndex, fileLabel);
+    	nodeIndex = 2;
+    	fileLabel = "parameters";
+            File paramsFile = new File(outputDirectory.getAbsolutePath() + "/parameters.xml");
+            this.addOutputFile(paramsFile, nodeIndex, fileIndex, fileLabel);
 
+        }               
         // Send the component output back to the workflow.
         System.out.println(this.getOutput());
+        
+    }
+    
+    private boolean isColumnNumeric(String filePath, String colName) {
+    	String[][] allCells = IOUtil.read2DStringArray(filePath, "\t");
+        String[] headers = allCells[0];
+        if (headers.length < 2) {
+        	allCells = IOUtil.read2DStringArray(filePath, ",");
+            headers = allCells[0];
+        }
+        int cumsumColInd = -1;
+        for (int i = 0; i < headers.length; i++) {
+            String header = headers[i];
+            header = header.trim();
+            //also delete the quotes if there is 
+            header = header.replaceAll("^\"|\"$", "");
+            if (header.equals(colName)) {
+            	cumsumColInd = i;
+            }
+        }
+        
+        if (cumsumColInd == -1)
+        	return false;
+        for (int i = 1; i < allCells.length; i++) {
+        	if (!isNumeric(allCells[i][cumsumColInd]))
+        		return false;
+        }
+        return true;
+    }
+    
+    //in r col name with space should replace space with a "."
+    private boolean isNumeric(String val) {
+    	boolean valIsNumber = false;
+    	try {
+        	Double.parseDouble(val);
+        	valIsNumber = true;
+        } catch (NumberFormatException e) {
+        } finally {
+        	return valIsNumber;
+        }
     }
 }
